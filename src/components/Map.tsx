571e72a9a82757232681474b5d45e4ccbed38eaa
Ville-Veikko Saari <ville-veikko.saari@cgi.com>
2018-07-03 16:42:46 +0300

    basic map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,38 +1,91 @@
-import {LatLng} from 'leaflet'
+import * as classNames from 'classnames'
+import * as L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 import {inject, observer} from 'mobx-react'
 import * as React from 'react'
+import fullScreenEnterIcon from '../icons/icon-fullscreen-enter.svg'
+import fullScreenExitIcon from '../icons/icon-fullscreen-exit.svg'
 import {MapStore} from '../stores/mapStore'
-import LeafletMap from './LeafletMap'
+import './Map.css'
 
-
-interface IMapState {
-  center?: LatLng
-  isFullScreen: boolean
-  selectedRoutes: any[]
-}
 
 interface IMapProps {
   mapStore?: MapStore
 }
 
+
+const addControlButton = (map: L.Map, toggleFullscreen: () => void) => {
+  const fullScreenControl = L.Control.extend({
+    onAdd: () => {
+      const icon = L.DomUtil.create('img')
+      const container = L.DomUtil.create('button', 'leaflet-bar leaflet-control')
+      icon.setAttribute('src', fullScreenEnterIcon)
+      icon.setAttribute('height', '11')
+      icon.setAttribute('width', '11')
+      // container.className = styles.fullScreenButton
+      container.appendChild(icon)
+      container.onclick = () => {
+        icon.setAttribute('src', toggleFullscreen() ? fullScreenExitIcon : fullScreenEnterIcon)
+      }
+      return container
+    },
+    options: {
+      position: 'topleft',
+    },
+  })
+  map.addControl(new fullScreenControl())
+}
+
 @inject('mapStore')
 @observer
-class Map extends React.Component<IMapProps, IMapState> {
+class Map extends React.Component<IMapProps> {
+  private map: L.Map
+
   constructor(props: any) {
     super(props)
-    this.state = {
-      isFullScreen: false,
-      selectedRoutes: []
-    }
+    this.initializeMap = this.initializeMap.bind(this)
+    this.updateMap = this.updateMap.bind(this)
+  }
+
+  public componentDidMount() {
+    this.initializeMap()
+  }
+
+  public componentWillReact() {
+    this.updateMap()
+    global.console.log('willreact', this.props.mapStore!.isFullscreen)
+  }
+
+  public initializeMap() {
+    this.map = L.map('map-leaflet')
+    this.map.setView(this.props.mapStore!.coordinates, 15)
+    L.tileLayer('https://digitransit-prod-cdn-origin.azureedge.net/map/v1/hsl-map/{z}/{x}/{y}{retina}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      baseLayer: true,
+      maxZoom: 18,
+      retina: L.Browser.retina ? '' : '@2x',
+      tileSize: 512,
+      zoomOffset: -1,
+    }).addTo(this.map)
+    addControlButton(this.map, () => {this.props.mapStore!.toggleFullscreen()})
   }
 
   public render() {
+    global.console.log(this.props.mapStore!.isFullscreen)
     return (
-      <div>
-        <LeafletMap
-          center={this.props.mapStore!.getCoordinate()}
-        />
-      </div>)
+      <div
+        id='map-leaflet'
+        className={classNames('root', {fullscreen: this.props.mapStore!.isFullscreen})}
+      >
+        Leaflet
+      </div>
+    )
+  }
+
+  private updateMap() {
+    this.map.flyTo(this.props.mapStore!.coordinates)
   }
 }
 
--- src/stores/mapStore.ts
+++ src/stores/mapStore.ts
@@ -1,19 +1,44 @@
 import {LatLng} from 'leaflet'
-import {action, observable} from 'mobx'
+import {action, computed, observable} from 'mobx'
 
 export class MapStore {
-  @observable private coordinate: LatLng
+  @observable private _coordinates: LatLng
+  @observable private _isFullscreen: boolean
+  @observable private _routes: MapRoute[]
 
-  @action public setCoordinate(lat: number, lon: number) {
-    this.coordinate = new LatLng(lat, lon)
+  constructor(
+    coordinate = new LatLng(60.24, 24.9),
+    isFullscreen = false
+  ) {
+    this._coordinates = coordinate
+    this._isFullscreen = isFullscreen
+    this._routes = []
   }
 
-  public getCoordinate(): LatLng {
-    return this.coordinate
+  @computed get isFullscreen(): boolean {
+    return this._isFullscreen
+  }
+
+  @computed get routes(): MapRoute[] {
+    return this._routes
+  }
+
+  @computed get coordinates(): LatLng {
+    return this._coordinates
+  }
+
+  @action
+  public setCoordinates(lat: number, lon: number) {
+    this._coordinates = new LatLng(lat, lon)
+  }
+
+  @action
+  public toggleFullscreen() {
+    global.console.log(this._isFullscreen)
+    this._isFullscreen = !this._isFullscreen
   }
 }
 
 const observableMapStore = new MapStore()
-observableMapStore.setCoordinate(60.24, 24.9)
 
 export default observableMapStore
