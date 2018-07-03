440d089f007ac7a775932cefd9a78fc652c55a83
Ville-Veikko Saari <ville-veikko.saari@cgi.com>
2018-07-03 09:42:36 +0300

    testing out mobx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,5 @@
+import {inject} from 'mobx-react'
+import DevTools from 'mobx-react-devtools'
 import * as React from 'react'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 import './App.css'
@@ -6,13 +8,16 @@ import Map from './Map'
 
 const rootPath: string = '/'
 
-class App extends React.Component {
+@inject('mapStore')
+class App extends React.Component<any, any> {
 
   public render(): any {
     return (
       <Router>
         <div>
-          <Map/>
+
+          <DevTools />
+          <Map />
           <nav>
             <Link to={'/'}>/</Link>&nbsp;
             <Link to={'/login'}>Login</Link>
---
+++ src/models/ICoordinate.ts
@@ -0,0 +1,4 @@
+export default interface ICoordinate {
+  lat: number,
+  lon: number
+}
