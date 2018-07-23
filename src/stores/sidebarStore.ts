c7325d69a28669ae958fb6e0400381052876d4c5
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-07-23 13:06:52 +0300

    Create interface for SelectedLine and initialise the property as a empty array

--- src/stores/sidebarStore.ts
+++ src/stores/sidebarStore.ts
@@ -1,11 +1,20 @@
 import {action, computed, observable} from 'mobx'
 
+interface ISelectedLine {
+    lintunnus: string,
+    linverkko: string,
+    reitunnus: string
+}
+
 export class SidebarStore {
     @observable private _filters: string[]
-    @observable private _selectedLines: [{lintunnus: string, linverkko: string, reitunnus: string}]
+    @observable private _selectedLines: ISelectedLine[]
 
+    constructor() {
+        this._selectedLines = new Array<ISelectedLine>()
+    }
 
-    @computed get selectedLines(): [{lintunnus: string, linverkko: string, reitunnus: string}] {
+    @computed get selectedLines(): ISelectedLine[] {
         return this._selectedLines
     }
 
@@ -14,20 +23,19 @@ export class SidebarStore {
     }
 
     @action
-    public setSelectedLine(node: {lintunnus: string, linverkko: string, reitunnus: string}) {
+    public setSelectedLine(node: ISelectedLine) {
         this._selectedLines.push(node)
     }
 
     @action
     public removeSelectedLines() {
-        this._selectedLines = new Array() as [{lintunnus: string, linverkko: string, reitunnus: string}]
+        this._selectedLines = new Array<ISelectedLine>()
     }
 
     @action
     public setFilters(filters: string[]) {
         this._filters = filters
     }
-
 }
 
 const observableSidebarStore = new SidebarStore()
