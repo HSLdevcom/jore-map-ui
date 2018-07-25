38eb3391d0dda7759d7543b15d3d29f5a39b339c
niemijoe <niemijoe@hotmail.com>
2018-07-25 16:36:08 +0300

    started using models, fixed indent

--- src/stores/sidebarStore.ts
+++ src/stores/sidebarStore.ts
@@ -1,41 +1,37 @@
 import {action, computed, observable} from 'mobx'
 
-interface ISelectedLine {
-    lintunnus: string,
-    linverkko: string,
-    reitunnus: string
-}
+import {ISelectedLine} from '../models'
 
 export class SidebarStore {
-    @observable private _filters: string[]
-    @observable private _selectedLines: ISelectedLine[]
+  @observable private _filters: string[]
+  @observable private _selectedLines: ISelectedLine[]
 
-    constructor() {
-        this._selectedLines = new Array<ISelectedLine>()
-    }
+  constructor() {
+    this._selectedLines = new Array<ISelectedLine>()
+  }
 
-    @computed get selectedLines(): ISelectedLine[] {
-        return this._selectedLines
-    }
+  @computed get selectedLines(): ISelectedLine[] {
+    return this._selectedLines
+  }
 
-    @computed get filters(): string[] {
-        return this._filters
-    }
+  @computed get filters(): string[] {
+    return this._filters
+  }
 
-    @action
-    public setSelectedLine(node: ISelectedLine) {
-        this._selectedLines.push(node)
-    }
+  @action
+  public setSelectedLine(node: ISelectedLine) {
+    this._selectedLines.push(node)
+  }
 
-    @action
-    public removeSelectedLines() {
-        this._selectedLines = new Array<ISelectedLine>()
-    }
+  @action
+  public removeSelectedLines() {
+    this._selectedLines = new Array<ISelectedLine>()
+  }
 
-    @action
-    public setFilters(filters: string[]) {
-        this._filters = filters
-    }
+  @action
+  public setFilters(filters: string[]) {
+    this._filters = filters
+  }
 }
 
 const observableSidebarStore = new SidebarStore()
