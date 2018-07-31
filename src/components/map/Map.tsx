84e558b3302c45c4347e1c778228c341849097cd
Ville-Veikko Saari <ville-veikko.saari@cgi.com>
2018-07-31 20:31:54 +0300

    coordinates on map, unstyled

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,11 +1,12 @@
 import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { inject, observer } from 'mobx-react';
-import { autorun } from 'mobx';
 import * as React from 'react';
 import fullScreenEnterIcon from '../../icons/icon-fullscreen-enter.svg';
 import fullScreenExitIcon from '../../icons/icon-fullscreen-exit.svg';
 import { MapStore } from '../../stores/mapStore';
+import { autorun } from 'mobx';
+import CoordinateSystem from '../../enums/coordinateSystems';
 import { RouteStore } from '../../stores/routeStore';
 import RouteLayerView from '../../layers/routeLayerView';
 import * as s from './map.scss';
@@ -21,6 +22,11 @@ interface IMapProps {
 class Map extends React.Component<IMapProps> {
     private map: L.Map;
     private lastCenter: L.LatLng;
+    private coordinateControl: L.Control;
+    private xInput: HTMLInputElement;
+    private yInput: HTMLInputElement;
+    private xButton: HTMLElement;
+    private yButton: HTMLElement;
     private routeLayerView: RouteLayerView;
 
     constructor(props: IMapProps) {
@@ -29,13 +35,15 @@ class Map extends React.Component<IMapProps> {
 
     public componentDidMount() {
         this.initializeMap();
+        autorun(() => this.updateMap());
         this.routeLayerView = new RouteLayerView(this.map);
         autorun(() => this.updateRouteLines());
     }
 
-    public componentWillReact() {
-        this.updateMap();
-    }
+    // public componentWillReact() {
+    //     window.console.log('WILL REACT');
+    //     this.updateMap();
+    // }
 
     private updateRouteLines() {
         this.routeLayerView.drawRouteLines(this.props.routeStore!.openRoutes);
@@ -57,7 +65,7 @@ class Map extends React.Component<IMapProps> {
     }
 
     private initializeMap = () => {
-        this.map = L.map(s.mapLeaflet);
+        this.map = L.map(s.mapLeaflet, { zoomControl: false });
         this.lastCenter = this.props.mapStore!.coordinates;
         this.map.setView(this.props.mapStore!.coordinates, 15);
         // tslint:disable-next-line:max-line-length
@@ -73,6 +81,11 @@ class Map extends React.Component<IMapProps> {
             zoomOffset: -1,
         }).addTo(this.map);
         this.map.addControl(this.fullscreenControlButton());
+        this.coordinateControl = this.coordinateControlBox();
+        this.map.addControl(this.coordinateControl);
+        L.control.zoom({ position:'bottomright' }).addTo(this.map);
+        this.map.on('moveend', this.setMapCenterAsCenter);
+
     }
 
     private fullscreenControlButton = () => {
@@ -80,8 +93,9 @@ class Map extends React.Component<IMapProps> {
         fullscreenControl.onAdd = () => {
             const icon = L.DomUtil.create('img');
             const container = L.DomUtil.create('button', 'leaflet-bar leaflet-control');
+            container.id = 'fullscreenControl';
             icon.setAttribute('src', fullScreenEnterIcon);
-            icon.className = s.fullscreenButtonImg;
+            icon.className = s.fullscreenButton;
             container.className = s.fullscreenButton;
             container.appendChild(icon);
             container.onclick = () => {
@@ -90,14 +104,69 @@ class Map extends React.Component<IMapProps> {
                     'src',
                     this.props.mapStore!.isMapFullscreen
                         ? fullScreenExitIcon : fullScreenEnterIcon);
+                this.map.invalidateSize();
             };
+            L.DomEvent.disableClickPropagation(container);
             return container;
         };
-        fullscreenControl.options = { position: 'topleft' };
+        fullscreenControl.options = { position: 'bottomright' };
         return fullscreenControl;
     }
 
+    private coordinateControlBox = () => {
+        const coordinateControl = new L.Control();
+        coordinateControl.onAdd = () => {
+            const [lat, lon] = CoordinateSystem
+                .convertToCoordinateSystem(this.props.mapStore!.lat,
+                                           this.props.mapStore!.lon,
+                                           this.props.mapStore!.coordinateSystem);
+            const container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+            container.id = 'coordinateControl';
+            const xDiv = L.DomUtil.create('div');
+            this.xButton = L.DomUtil.create('button');
+            this.xButton.innerText = 'Lat';
+            this.xInput = L.DomUtil.create('input', '') as HTMLInputElement;
+            this.xInput.id = 'xcoord';
+            xDiv.appendChild(this.xButton);
+            xDiv.appendChild(this.xInput);
+            this.xInput.value = lat.toString(10);
+
+            const yDiv = L.DomUtil.create('div');
+            this.yButton = L.DomUtil.create('button');
+            this.yButton.innerText = 'Lon';
+            this.yInput = L.DomUtil.create('input', '') as HTMLInputElement;
+            this.yInput.id = 'ycoord';
+            yDiv.appendChild(this.yButton);
+            yDiv.appendChild(this.yInput);
+            container.appendChild(xDiv);
+            container.appendChild(yDiv);
+            this.yInput.value = lon.toString(10);
+
+            this.xInput.onblur = this.yInput.onblur = (e) => {
+                const newX = Number(this.xInput.value);
+                const newY = Number(this.yInput.value);
+                if (!isNaN(newY) && !isNaN(newX)) {
+                    this.setInputAsCenter(newX, newY);
+                }
+            };
+            this.xButton.onclick = this.yButton.onclick = () => {
+                this.props.mapStore!.coordinateSystem =
+                    CoordinateSystem.nextCoordinateSystem(this.props.mapStore!.coordinateSystem);
+            };
+            L.DomEvent.disableClickPropagation(container);
+            return container;
+        };
+        return coordinateControl;
+    }
+
     private updateMap = () => {
+        [this.xInput.value, this.yInput.value] = CoordinateSystem
+            .convertToCoordinateSystem(this.props.mapStore!.lat,
+                                       this.props.mapStore!.lon,
+                                       this.props.mapStore!.coordinateSystem)
+            .map(val => val.toString(10));
+        ({ x: this.xButton.innerText, y: this.yButton.innerText } =
+            CoordinateSystem.coordinateNames(this.props.mapStore!.coordinateSystem));
         if (!this.lastCenter.equals(this.props.mapStore!.coordinates)) {
             this.map.flyTo(this.props.mapStore!.coordinates);
             this.lastCenter = new L.LatLng(
@@ -105,6 +174,16 @@ class Map extends React.Component<IMapProps> {
                 this.props.mapStore!.coordinates.lng,
             );
         }
+    }
+
+    private setMapCenterAsCenter = () =>
+        this.props.mapStore!.setCoordinates(this.map.getCenter().lat, this.map.getCenter().lng)
+
+    private setInputAsCenter = (lat: number, lon: number) => {
+        const [latt, lonn] =
+            CoordinateSystem.convertToCoordinateSystem(
+                lat, lon, CoordinateSystem.EPSG4326, this.props.mapStore!.coordinateSystem);
+        this.props.mapStore!.setCoordinates(latt, lonn);
     }
 }
 
--- src/stores/mapStore.ts
+++ src/stores/mapStore.ts
@@ -1,10 +1,13 @@
 import { LatLng } from 'leaflet';
 import { action, computed, observable } from 'mobx';
+import CoordinateSystem from '../enums/coordinateSystems';
 
 export class MapStore {
+
     @observable private _coordinates: LatLng;
     @observable private _isMapFullscreen: boolean;
     @observable private _routes: MapRoute[];
+    @observable private _coordinateSystem:CoordinateSystem;
 
     constructor(
         coordinate = new LatLng(60.24, 24.9),
@@ -13,6 +16,7 @@ export class MapStore {
         this._coordinates = coordinate;
         this._isMapFullscreen = isFullscreen;
         this._routes = [];
+        this._coordinateSystem = CoordinateSystem.EPSG4326;
     }
 
     @computed get isMapFullscreen(): boolean {
@@ -27,6 +31,22 @@ export class MapStore {
         return this._coordinates;
     }
 
+    @computed get lat(): number {
+        return this._coordinates.lat;
+    }
+
+    set lat(lat: number) {
+        this._coordinates.lat = lat;
+    }
+
+    @computed get lon(): number {
+        return this._coordinates.lng;
+    }
+
+    set lon(lon: number) {
+        this._coordinates.lng = lon;
+    }
+
     @action
     public setCoordinates(lat: number, lon: number) {
         this._coordinates = new LatLng(lat, lon);
@@ -35,6 +55,15 @@ export class MapStore {
     @action
     public toggleMapFullscreen = () => {
         this._isMapFullscreen = !this._isMapFullscreen;
+    }
+
+    @computed
+    get coordinateSystem(): CoordinateSystem {
+        return this._coordinateSystem;
+    }
+
+    set coordinateSystem(value: CoordinateSystem) {
+        this._coordinateSystem = value;
     }
 }
 
