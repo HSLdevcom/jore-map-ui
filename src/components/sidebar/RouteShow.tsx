3a918095f4f2c70fee710791b9772e5e705db85f
niemijoe <niemijoe@hotmail.com>
2018-08-06 15:08:23 +0300

    routeShow is onw in its own component

---
+++ src/components/sidebar/RouteShow.tsx
@@ -0,0 +1,66 @@
+import { inject, observer } from 'mobx-react';
+import * as React from 'react';
+import { RouteStore } from '../../stores/routeStore';
+import { IRoutePath, IRoute } from '../../models';
+import ToggleButton from '../controls/ToggleButton';
+import classNames from 'classnames';
+import LineHelper from '../../util/lineHelper';
+import TransitTypeColorHelper from '../../util/transitTypeColorHelper';
+import * as s from './routeShow.scss';
+
+interface IRouteShowProps {
+    routeStore?: RouteStore;
+    route: IRoute;
+}
+
+@inject('routeStore')
+@observer
+class RouteShow extends React.Component<IRouteShowProps> {
+    public render(): any {
+        return (
+            <span>
+                <span className={s.routeName}>
+                    {LineHelper.getTransitIcon(this.props.route.line.transitType, false)}
+                    <span
+                        className={classNames(
+                            s.label,
+                            TransitTypeColorHelper.getColorClass(
+                                this.props.route.line.transitType,
+                                false,
+                            ),
+                        )}
+                    >
+                        {this.props.route.routeName}
+                    </span>
+                </span>
+                {this.props.route.routePaths
+                    .sort((a, b) => a.lastModified.getTime() - b.lastModified.getTime())
+                    .map((routePath: IRoutePath, index: number) => {
+                        const toggleRoutePathVisibility = () => {
+                            this.props.routeStore!.toggleRoutePathVisibility(
+                                this.props.route, routePath);
+                        };
+
+                        return (
+                            <div
+                                className={s.toggle}
+                                key={`${routePath.directionName}-${index}`}
+                            >
+                                <span className={s.toggleTitle}>
+                                    Suunta {routePath.direction}
+                                </span>
+                                <ToggleButton
+                                    onClick={toggleRoutePathVisibility}
+                                    value={routePath.visible}
+                                    type={this.props.route.line.transitType}
+                                />
+                            </div>
+                        );
+                    })
+                }
+            </span>
+        );
+    }
+}
+
+export default RouteShow;
