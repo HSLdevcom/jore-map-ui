54c00ca3d2456d333bf91c41befd28f3f571b1ab
Anton Jyrkiäinen <anton.jyrkiainen@karttakeskus.fi>
2018-08-20 12:46:51 +0300

    better starting points

--- src/factories/routeFactory.ts
+++ src/factories/routeFactory.ts
@@ -1,25 +1,10 @@
-import { IRoute, IRoutePath, ILine, INode } from '../models';
-import NodeType from '../enums/nodeType';
+import { IRoute, IRoutePath, ILine } from '../models';
 import RoutePathFactory from './routePathFactory';
 
 class RouteFactory {
-
-    private static addStartNodes = (routePaths: IRoutePath[]) => {
-        for (let i = 0; i < routePaths.length; i = i + 1) {
-            const startPoint = routePaths[i].nodes[0];
-            const routeStart: INode = {
-                id: 0,
-                type: NodeType.START,
-                geoJson: startPoint.geoJson,
-            };
-            routePaths[i].nodes.unshift(routeStart);
-        }
-        return routePaths;
-    }
-
     // reitti to IRoute
     public static createRoute = (reitti: any, line?: ILine): IRoute => {
-        let routePaths:IRoutePath[]
+        const routePaths:IRoutePath[]
             = reitti.reitinsuuntasByReitunnus.edges
                 .map((routePath: any, index:number) => {
                     // By default make the first two routePaths visible
@@ -28,8 +13,6 @@ class RouteFactory {
                     return RoutePathFactory.createRoutePath(routePath.node, isVisible);
                 });
 
-        routePaths = RouteFactory.addStartNodes(routePaths);
-        console.log(routePaths);
         return <IRoute>{
             routePaths,
             line,
--- src/factories/routePathFactory.ts
+++ src/factories/routePathFactory.ts
@@ -1,18 +1,38 @@
 import { INode, IRoutePath } from '../models';
 import NodeFactory from './nodeFactory';
+import NodeType from '../enums/nodeType';
 
 class RoutePathFactory {
+
+    private static getStartNode = (geoJson: any) => {
+        const startingPointNode: INode = {
+            id: 0,
+            type: NodeType.START,
+            geoJson: {
+                type: NodeType.START,
+                coordinates: geoJson.coordinates[0],
+            },
+        };
+        return startingPointNode;
+    }
+
     // suunta to IRoutePath
     public static createRoutePath = (suunta: any, isVisible:boolean): IRoutePath => {
         const nodes:INode[]
         = suunta.reitinlinkkisByReitunnusAndSuuvoimastAndSuusuunta.edges
-            .map((node: any) => NodeFactory.createNode(node.node));
+            .map((node: any) => {
+                return NodeFactory.createNode(node.node);
+            });
+
+        const geoJson = JSON.parse(suunta.geojson);
+        const startingPointNode = RoutePathFactory.getStartNode(geoJson);
+        nodes.unshift(startingPointNode);
 
         return <IRoutePath>{
             nodes,
+            geoJson,
             routePathName: suunta.suunimi,
             direction: suunta.suusuunta,
-            geoJson: JSON.parse(suunta.geojson),
             startTime: new Date(suunta.suuvoimast),
             endTime: new Date(suunta.suuviimpvm),
             lastModified: new Date(suunta.suuvoimviimpvm),
