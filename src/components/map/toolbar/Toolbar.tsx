bc1bf5a9012646a0b9de227facb08cbcfbe956de
niemijoe <niemijoe@hotmail.com>
2018-09-11 15:55:39 +0300

    removed unused toolbarStore

--- src/components/map/toolbar/Toolbar.tsx
+++ src/components/map/toolbar/Toolbar.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { ToolbarStore } from '../../../stores/toolbarStore';
 import ToolbarModeButtons from './toolbarModeButtons';
 import ToolbarToolButtons from './toolbarToolButtons';
 import * as s from './toolbar.scss';
 
-interface IToolbarProps {
-    toolbarStore?: ToolbarStore;
-}
-
 @observer
-export default class Toolbar extends React.Component<IToolbarProps> {
-    constructor(props: IToolbarProps) {
-        super(props);
-    }
+export default class Toolbar extends React.Component {
     render() {
         return (
             <div className={s.toolbarView}>
