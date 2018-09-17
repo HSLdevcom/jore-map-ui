fcdc921592b29201e5b5bfb2e1a49b03d6b4e044
niemijoe <joe.niemi@cgi.com>
2018-09-17 10:10:10 +0300

    linkView implemented

---
+++ src/components/sidebar/LinkView.tsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { inject, observer } from 'mobx-react';
+import classnames from 'classnames';
+import { SidebarStore } from '../../stores/sidebarStore';
+import * as s from './linkView.scss';
+
+interface ILinkViewState {
+}
+
+interface ILinkViewProps {
+    sidebarStore?: SidebarStore;
+}
+
+@inject('sidebarStore')
+@observer
+class LinkView extends React.Component
+<ILinkViewProps, ILinkViewState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    public componentDidMount() {
+        if (this.props.sidebarStore) {
+            // TODO: fetch GraphSQL with linkId
+            // const linkId = this.props.sidebarStore!.openedLinkId;
+        }
+    }
+
+    private closeLinkView = () => {
+        this.props.sidebarStore!.setOpenLinkId(null);
+    }
+
+    public render(): any {
+        return (
+        <div className={s.linkView}>
+            <div className={s.header}>
+                <div className={s.topic}>Reitin 1016 linkki</div>
+                <div
+                    className={s.closeButton}
+                    onClick={this.closeLinkView}
+                />
+            </div>
+            <div className={classnames(s.flexInnerColumn, s.subTopic)}>
+                REITIN SUUNNAN TIEDOT
+            </div>
+            <div className={s.flexInnerColumn}>
+                <div className={s.flexInnerRow}>
+                    <div>
+                        <div className={classnames(s.subTopic)}>
+                            REITTITUNNUS
+                        </div>
+                        <input
+                            placeholder='1016'
+                            type='text'
+                            className={s.inputField}
+                        />
+                    </div>
+                    <div>
+                        <div className={classnames(s.subTopic)}>
+                            SUUNTA
+                        </div>
+                        <input
+                            placeholder='Suunta 1'
+                            type='text'
+                            className={s.inputField}
+                        />
+                    </div>
+                </div>
+                <div className={s.flexInnerRow}>
+                    <div>
+                        <div className={classnames(s.subTopic)}>
+                            VOIM. AST
+                        </div>
+                        <input
+                            placeholder='01.09.2017'
+                            type='text'
+                            className={s.inputField}
+                        />
+                    </div>
+                    <div>
+                        <div className={classnames(s.subTopic)}>
+                            VIIM. VOIM
+                        </div>
+                        <input
+                            placeholder='31.12.2050'
+                            type='text'
+                            className={s.inputField}
+                        />
+                    </div>
+                </div>
+                <div className={s.flexInnerColumn}>
+                    <div className={classnames(s.subTopic)}>
+                        NIMI
+                    </div>
+                    <input
+                        placeholder='Rautatientori - Korkeasaari'
+                        type='text'
+                        className={s.inputField}
+                    />
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+export default LinkView;
--- src/stores/sidebarStore.ts
+++ src/stores/sidebarStore.ts
@@ -2,7 +2,7 @@ import { action, computed, observable } from 'mobx';
 
 export class SidebarStore {
     @observable private _openNodeId: number|null;
-    @observable private _openLinkId: number|null;
+    @observable private _openLinkId: string|null;
 
     constructor() {
         this._openNodeId = null;
@@ -15,18 +15,20 @@ export class SidebarStore {
     }
 
     @computed
-    get openLinkId(): number|null {
+    get openLinkId(): string|null {
         return this._openLinkId;
     }
 
     @action
     public setOpenNodeId(id: number|null) {
         this._openNodeId = id;
+        if (this._openLinkId) this._openLinkId = null;
     }
 
     @action
-    public setOpenLinkId(id: number|null) {
+    public setOpenLinkId(id: string|null) {
         this._openLinkId = id;
+        if (this._openNodeId) this._openNodeId = null;
     }
 
 }
