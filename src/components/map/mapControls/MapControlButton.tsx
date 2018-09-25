98b65c0cb67dbeaa25a20b00f89a6c913faac8fa
Ville-Veikko Saari <ville-veikko.saari@cgi.com>
2018-09-25 16:32:07 +0300

    Renamed MapControlButton props

--- src/components/map/mapControls/MapControlButton.tsx
+++ src/components/map/mapControls/MapControlButton.tsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import * as s from './mapControlButton.scss';
 
-interface ToolbarButtonProps {
+interface MapControlButtonProps {
     label: string;
     onClick: () => void;
     isActive: boolean;
     isDisabled: boolean;
 }
 
-export default class MapControlButton extends Component<ToolbarButtonProps>{
+export default class MapControlButton extends Component<MapControlButtonProps>{
     render () {
         const classes = classnames(
             s.mapControlButton,
