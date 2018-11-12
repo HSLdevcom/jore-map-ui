8bdf3597df920031ee51d910617e310cc89d7d10
Joakim Kronqvist <kronken@users.noreply.github.com>
2018-11-12 13:34:51 +0200

    351 Make FE complient with BE saving logic (#366)
    * Make FE compliant with BE saving logic (date formats, error codes and such)
    
    * Commenting, and port fix
    
    * PR fixes
    
    * Remove console logs

--- src/util/ApiClient.tsx
+++ src/util/ApiClient.tsx
@@ -1,9 +1,16 @@
 import entityNames from '~/enums/entityNames';
+import FetchStatusCode from '~/enums/fetchStatusCode';
+import ApiClientHelper from './apiClientHelper';
 
 enum RequestMethod {
     POST= 'POST',
     PUT= 'PUT',
     DELETE= 'DELETE',
+}
+
+export class IRequestError {
+    errorCode: FetchStatusCode;
+    message: string;
 }
 
 const API_URL = process.env.API_URL || 'http://localhost:3040';
@@ -22,14 +29,36 @@ export default class ApiClient {
     }
 
     private async sendRequest(method: RequestMethod, entityName: entityNames, object: any) {
-        return await fetch(this.getUrl(entityName), {
-            method,
-            body: JSON.stringify(object),
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        });
+        const formattedObject = ApiClientHelper.format(object);
+        let error : (IRequestError | null) = null;
+
+        try {
+            const response = await fetch(this.getUrl(entityName), {
+                method,
+                body: JSON.stringify(formattedObject),
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.status >= 200 && response.status < 300) {
+                return;
+            }
+            error = {
+                errorCode: response.status,
+                message: response.statusText,
+            };
+        } catch (err) {
+            error = {
+                errorCode: FetchStatusCode.CONNECTION_ERROR,
+                message: 'Yhteysongelma',
+            };
+        }
+
+        if (error) {
+            throw error;
+        }
     }
 
     private getUrl(entityName: entityNames) {
