18028990940dd7578cc2962a94c258d28c7c7a31
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-11-19 13:38:43 +0200

    Paint Hastus ID on stops when zoom is higher or equal to 16

--- src/components/map/layers/NodeLayer.tsx
+++ src/components/map/layers/NodeLayer.tsx
@@ -23,16 +23,22 @@ interface MarkerLayerProps {
 }
 
 const DEFAULT_RADIUS = 25;
+const HASTUS_MIN_ZOOM = 16;
 
 @inject('popupStore', 'toolbarStore', 'sidebarStore', 'mapStore')
 @observer
 export default class NodeLayer extends Component<MarkerLayerProps> {
-    private getMarkerHtml = (markerClass: string) => {
-        return `<div class="${classnames(s.nodeBase, markerClass)}" />`;
+    private getMarkerHtml = (markerClass: string, children: string[]) => {
+        return `<div class="${classnames(s.nodeBase, markerClass)}">
+                ${
+                    children
+                }
+            </div>`;
     }
 
     private getIcon = (node: INode) => {
         const isSelected = this.isSelected(node);
+        const children = [];
 
         let type;
         if (this.props.isDisabled) {
@@ -43,31 +49,40 @@ export default class NodeLayer extends Component<MarkerLayerProps> {
             type = node.type;
         }
 
+        if (node.stop && node.stop.hastusId && this.props.mapStore!.zoom >= HASTUS_MIN_ZOOM) {
+            children.push(
+                `<div class=${s.hastusId}>${node.stop.hastusId}</div>`,
+            );
+        }
+
         let html;
         switch (type) {
         case NodeType.STOP: {
-            html = this.getMarkerHtml(isSelected ? s.stopMarkerHighlight : s.stopMarker);
+            html = this.getMarkerHtml(isSelected ? s.stopMarkerHighlight : s.stopMarker, children);
             break;
         }
         case NodeType.CROSSROAD: {
-            html = this.getMarkerHtml(isSelected ? s.crossroadMarkerHighlight : s.crossroadMarker);
+            html = this.getMarkerHtml(
+                isSelected ? s.crossroadMarkerHighlight : s.crossroadMarker, children);
             break;
         }
         case NodeType.MUNICIPALITY_BORDER: {
-            html = this.getMarkerHtml(isSelected ?
-                s.municipalityMarkerHighlight : s.municipalityMarker);
+            html = this.getMarkerHtml(
+                isSelected ? s.municipalityMarkerHighlight : s.municipalityMarker, children);
             break;
         }
         case NodeType.DISABLED: {
-            html = this.getMarkerHtml(isSelected ? s.disabledMarkerHighlight : s.disabledMarker);
+            html = this.getMarkerHtml(
+                isSelected ? s.disabledMarkerHighlight : s.disabledMarker, children);
             break;
         }
         case NodeType.TIME_ALIGNMENT: {
-            html = this.getMarkerHtml(s.timeAlignmentMarker);
+            html = this.getMarkerHtml(s.timeAlignmentMarker, children);
             break;
         }
         default: {
-            html = this.getMarkerHtml(isSelected ? s.unknownMarkerHighlight : s.unknownMarker);
+            html = this.getMarkerHtml(
+                isSelected ? s.unknownMarkerHighlight : s.unknownMarker, children);
             break;
         }
         }
