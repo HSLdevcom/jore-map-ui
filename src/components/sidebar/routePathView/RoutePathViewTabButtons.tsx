586d9b395700841832d66bf306cad50932dcabc5
Anton Jyrkiäinen <anton.jyrkiainen@gmail.com>
2018-12-11 11:58:14 +0200

    style changes, routepathviewtabbuttons to static component

--- src/components/sidebar/routePathView/RoutePathViewTabButtons.tsx
+++ src/components/sidebar/routePathView/RoutePathViewTabButtons.tsx
@@ -2,43 +2,31 @@ import * as React from 'react';
 import classnames from 'classnames';
 import * as s from './routePathViewTabButtons.scss';
 
-interface IRoutePathViewTabButtonsState {
-}
-
 interface IRoutePathViewTabButtonsProps {
     tabs: String[];
     selectedTab: string;
     onClick: Function;
 }
 
-class RoutePathViewTabButtons extends React.Component
-<IRoutePathViewTabButtonsProps, IRoutePathViewTabButtonsState>{
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            routePath: null,
-            isLoading: true,
-            selectedTab: 'Reitinsuunta',
-        };
-    }
-
-    public render(): any {
-        return this.props.tabs.map((tab: string) => {
+const routePathViewTabButtons = (props: IRoutePathViewTabButtonsProps) => {
+    return (
+        <> {props.tabs.map((tab: string) => {
             return(
                 <div
                     key={tab}
-                    className={(this.props.selectedTab === tab) ?
+                    className={(props.selectedTab === tab) ?
                     classnames(s.routePathViewTabButtonsView, s.selected) :
                     s.routePathViewTabButtonsView}
-                    onClick={this.props.onClick(tab)}
+                    onClick={props.onClick(tab)}
                 >
                     <div className={s.tabLabel}>
                         {tab}
                     </div>
                 </div>
             );
-        });
-    }
-}
+        })}
+        </>
+    );
+};
 
-export default RoutePathViewTabButtons;
+export default routePathViewTabButtons;
