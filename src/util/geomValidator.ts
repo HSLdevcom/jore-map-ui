3790891607208577c1060e492cdae646dbb3c009
Ville-Veikko Saari <ville-veikko.saari@cgi.com>
2019-01-18 11:09:33 +0200

    Simplified validation function

--- src/util/geomValidator.ts
+++ src/util/geomValidator.ts
@@ -1,16 +1,8 @@
 import { IRoutePathLink } from '~/models';
 
 const validateRoutePathLinks = (rpLinks: IRoutePathLink[]) => {
-    let valid = true;
-    rpLinks.forEach((rpLink, index) => {
-        if (index !== 0) {
-            const previousLink = rpLinks[index - 1];
-            if (previousLink.endNode.id !== rpLink.startNode.id) {
-                valid = false;
-            }
-        }
-    });
-    return valid;
+    return rpLinks.every((rpLink, index) => (
+        index === 0 || rpLinks[index - 1].endNode.id === rpLink.startNode.id));
 };
 
 export {
