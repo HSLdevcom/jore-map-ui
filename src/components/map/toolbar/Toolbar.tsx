33a700694140424c61dfc462799c716096c6146c
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-01-29 15:27:13 +0200

    Render suitable tools

--- src/components/map/toolbar/Toolbar.tsx
+++ src/components/map/toolbar/Toolbar.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { matchPath } from 'react-router';
 import classnames from 'classnames';
-import ToolbarLineButtons from './toolbarLineButtons';
+import navigator from '~/routing/navigator';
+import SubSites from '~/routing/subSites';
+import RoutePathButtons from './toolbarRoutePathButtons';
 import ToolbarCommonButtons from './ToolbarCommonButtons';
 import ToolbarHelp from './toolbarHelp';
 import * as s from './toolbar.scss';
 
 @observer
 class Toolbar extends React.Component {
+    private renderObjectSpecificTools = () => {
+        if (matchPath(navigator.getPathName(), SubSites.routePath)) {
+            return <RoutePathButtons />;
+        }
+        return null;
+    }
+
     render() {
         return (
             <div className={s.toolbarContainer}>
                 <div className={s.toolbarRow}>
                     <div className={classnames(s.toolbar, s.modeSpecificToolbar)}>
-                        <ToolbarLineButtons />
+                        {this.renderObjectSpecificTools()}
                     </div>
                     <div className={s.toolbar}>
                         <ToolbarCommonButtons />
