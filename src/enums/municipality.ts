90834f7a4f5e0a198fda958d61c1fd658d85b68b
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-01-30 08:45:24 +0200

    Add municipality mappings

--- src/enums/municipality.ts
+++ src/enums/municipality.ts
@@ -1,10 +1,35 @@
-enum Municipality {
-    Helsinki = '1',
-    Espoo = '2',
-    Vantaa = '3',
-    Kirkkonummi = '4',
-    Sipoo = '5',
-    // TODO: Replace with correct values
-}
+const municipalityDictionary = {
+    ['999']: 'Muu',
+    ['753']: 'Sipoo',
+    ['858']: 'Tuusula',
+    ['611']: 'Pornainen',
+    ['638']: 'Porvoo',
+    ['091']: 'Helsinki',
+    ['235']: 'Kauniainen',
+    ['755']: 'Siuntio',
+    ['245']: 'Kerava',
+    ['092']: 'Vantaa',
+    ['186']: 'Järvenpää',
+    ['257']: 'Kirkkonummi',
+    ['049']: 'Espoo',
+};
 
-export default Municipality;
+/* const municipalityDictionary = {
+    Muu = '999',
+    Sipoo = '753',
+    Tuusula = '858',
+    Pornainen = '611',
+    Porvoo = '638',
+    Helsinki = '091',
+    Kauniainen = '235',
+    Siuntio = '755',
+    Kerava = '245',
+    Vantaa = '092',
+    Järvenpää = '186',
+    Kirkkonummi = '257',
+    Espoo = '049',
+} */
+
+export default {
+    municipalityDictionary,
+};
