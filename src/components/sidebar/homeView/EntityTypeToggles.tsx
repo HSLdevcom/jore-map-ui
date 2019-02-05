5890535eac6d2dd0d36e20d292801aeb11f84956
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-05 08:50:36 +0200

    Add ugly UI for toggling between line and node searching

--- src/components/sidebar/homeView/EntityTypeToggles.tsx
+++ src/components/sidebar/homeView/EntityTypeToggles.tsx
@@ -1,12 +1,59 @@
 import React from 'react';
+import classnames from 'classnames';
+import { inject, observer } from 'mobx-react';
 import { IoMdAnalytics, IoMdLocate } from 'react-icons/io';
+import { SearchStore } from '~/stores/searchStore';
 import * as s from './entityTypeToggles.scss';
 
-const entityTypeToggles = () => (
-    <div className={s.entityTypeTogglesView}>
-        <IoMdAnalytics />
-        <IoMdLocate />
-    </div>
-);
+interface IEntityTypeToggleProps {
+    searchStore?: SearchStore;
+}
 
-export default entityTypeToggles;
+@inject('searchStore')
+@observer
+class EntityTypeToggles extends React.Component<IEntityTypeToggleProps> {
+    private toggleSearchingLines = () => {
+        this.props.searchStore!.toggleIsSearchingForLines();
+    }
+
+    private toggleSearchingNodes = () => {
+        this.props.searchStore!.toggleIsSearchingForNodes();
+    }
+
+    render() {
+        return (
+          <div className={s.entityTypeTogglesView}>
+            <div className={s.buttonContainer}>
+              <div
+                  className={classnames(
+                      s.button,
+                      this.props.searchStore!.isSearchingForLines ? s.active : null,
+                  )}
+                  onClick={this.toggleSearchingLines}
+              >
+              <IoMdAnalytics />
+                <div>
+                Linjat ja Reitit
+                </div>
+              </div>
+            </div>
+            <div className={s.buttonContainer}>
+              <div
+                className={classnames(
+                    s.button,
+                    this.props.searchStore!.isSearchingForNodes ? s.active : null,
+                )}
+                onClick={this.toggleSearchingNodes}
+              >
+                <IoMdLocate />
+                <div>
+                  Solmut
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+export default EntityTypeToggles;
