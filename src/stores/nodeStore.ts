7da492d226debd93ee005a8c1dda09b6732569ff
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-06 11:01:15 +0200

    Add initial new stores

---
+++ src/stores/nodeStore.ts
@@ -0,0 +1,41 @@
+import { action, computed, observable } from 'mobx';
+import { ILink, INode } from '~/models';
+
+export class NodeStore {
+    @observable private _links: ILink[];
+    @observable private _node?: INode;
+
+    constructor() {
+        this._links = [];
+    }
+
+    @computed
+    get links(): ILink[] {
+        return this._links;
+    }
+
+    @computed
+    get node(): INode {
+        return this._node!;
+    }
+
+    @action
+    public setLinks = (links: ILink[]) => {
+        this._links = links;
+    }
+
+    @action
+    public setNode = (node: INode) => {
+        this._node = node;
+    }
+
+    @action
+    public clear = () => {
+        this._links = [];
+        this._node = undefined;
+    }
+}
+
+const observableNodeStore = new NodeStore();
+
+export default observableNodeStore;
