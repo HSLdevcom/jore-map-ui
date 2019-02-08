af883d49cf9d318ed56b8e5673999d4ce8ae9a36
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-08 14:52:10 +0200

    Add initial validation model for nodes

---
+++ src/validation/models/nodeValidationModel.ts
@@ -0,0 +1,6 @@
+const nodeValidationModel = {
+    name: 'required|min:1|max:20|string', // TODO: These are just made up
+    longName: 'required|min:1|max:40|string', // TODO: These are just made up
+};
+
+export default nodeValidationModel;
