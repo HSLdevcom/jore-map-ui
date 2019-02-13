7562327213d02543af0cbd58cc38132cc8c82002
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-13 07:57:32 +0200

    Add save button

--- src/components/sidebar/networkView/linkView/LinkView.tsx
+++ src/components/sidebar/networkView/linkView/LinkView.tsx
@@ -65,6 +65,11 @@ class LinkView extends ViewFormBase<ILinkViewProps, ILinkViewState> {
     private onChange = () => {
     }
 
+    private save = async () => {
+        this.setState({ isLoading: true });
+        this.setState({ isLoading: false });
+    }
+
     private navigateToNode = (nodeId: string) => () => {
         const editNetworkLink = routeBuilder
             .to(SubSites.node)
@@ -100,208 +105,220 @@ class LinkView extends ViewFormBase<ILinkViewProps, ILinkViewState> {
         const startNode = link!.startNode;
         const endNode = link!.endNode;
         const datetimeStringDisplayFormat = 'YYYY-MM-DD HH:mm:ss';
+        const isSaveButtonDisabled = this.state.isEditingDisabled
+            || !this.props.linkStore!.isDirty;
+            // || !this.isFormValid();
 
         return (
         <div className={classnames(s.linkView)}>
-            <ViewHeader
-                closePromptMessage={
-                    this.props.linkStore!.isDirty ? closePromptMessage : undefined
-                }
-                isEditButtonVisible={true}
-                isEditing={!isEditingDisabled}
-                onEditButtonClick={this.toggleIsEditingEnabled}
-            >
-                Linkki
-            </ViewHeader>
-            <div className={s.formSection}>
-                <div className={s.flexRow}>
-                    <div className={s.flexInnerRowFlexEnd}>
-                        <InputContainer
-                            label='ALKUSOLMU'
-                            disabled={true}
-                            value={startNode ? startNode.id : '-'}
-                        />
-                        <Dropdown
-                            label='TYYPPI'
-                            disabled={true}
-                            selected={startNode.type}
-                            codeList={nodeTypeCodeList}
-                        />
-                        <InputContainer
-                            label='NIMI'
-                            disabled={true}
-                            value={
-                                startNode && startNode.stop ? startNode.stop!.nameFi : '-'}
-                        />
-                    </div>
-                </div>
-                <div className={s.flexRow}>
-                    <div className={s.flexInnerRowFlexEnd}>
-                        <InputContainer
-                            label='LOPPUSOLMU'
-                            disabled={true}
-                            value={endNode ? endNode.id : '-'}
-                        />
-                        <Dropdown
-                            label='TYYPPI'
-                            disabled={true}
-                            selected={endNode.type}
-                            codeList={nodeTypeCodeList}
-                        />
-                        <InputContainer
-                            label='NIMI'
-                            disabled={true}
-                            value={endNode && endNode.stop ? endNode.stop!.nameFi : '-'}
-                        />
-                    </div>
-                </div>
-                <div className={s.flexRow}>
-                    <div className={s.formItem}>
-                        <div className={s.inputLabel}>
-                            VERKKO
+            <div>
+                <ViewHeader
+                    closePromptMessage={
+                        this.props.linkStore!.isDirty ? closePromptMessage : undefined
+                    }
+                    isEditButtonVisible={true}
+                    isEditing={!isEditingDisabled}
+                    onEditButtonClick={this.toggleIsEditingEnabled}
+                >
+                    Linkki
+                </ViewHeader>
+                <div className={s.formSection}>
+                    <div className={s.flexRow}>
+                        <div className={s.flexInnerRowFlexEnd}>
+                            <InputContainer
+                                label='ALKUSOLMU'
+                                disabled={true}
+                                value={startNode ? startNode.id : '-'}
+                            />
+                            <Dropdown
+                                label='TYYPPI'
+                                disabled={true}
+                                selected={startNode.type}
+                                codeList={nodeTypeCodeList}
+                            />
+                            <InputContainer
+                                label='NIMI'
+                                disabled={true}
+                                value={
+                                    startNode && startNode.stop ? startNode.stop!.nameFi : '-'}
+                            />
                         </div>
-                        <TransitToggleButtonBar
-                            selectedTransitTypes={[link!.transitType]}
-                        />
                     </div>
-                </div>
-                <div className={s.flexRow}>
-                    <Dropdown
-                        label='SUUNTA'
-                        disabled={isEditingDisabled}
-                        selected={link.direction}
-                        codeList={directionCodeList}
-                    />
-                    <InputContainer
-                        label='OS. NRO'
-                        disabled={isEditingDisabled}
-                        value={link.osNumber}
-                    />
-                    <InputContainer
-                        label='LINKIN PITUUS (m)'
-                        disabled={isEditingDisabled}
-                        value={link.length}
-                    />
-                </div>
-                <div className={s.flexRow}>
-                    <InputContainer
-                        label='KATU'
-                        disabled={isEditingDisabled}
-                        value={link.streetName}
-                    />
-                    <InputContainer
-                        label='KATUOSAN OS. NRO'
-                        disabled={isEditingDisabled}
-                        value={link.streetNumber}
-                    />
-                    <Dropdown
-                        onChange={this.onChange}
-                        disabled={isEditingDisabled}
-                        codeList={municipalityCodeList}
-                        selected={link.municipalityCode}
-                        label='KUNTA'
-                    />
-                </div>
-                <div className={s.flexRow}>
-                    <div className={s.inputLabel}>
-                        ALKUSOLMUN SARAKE NRO
+                    <div className={s.flexRow}>
+                        <div className={s.flexInnerRowFlexEnd}>
+                            <InputContainer
+                                label='LOPPUSOLMU'
+                                disabled={true}
+                                value={endNode ? endNode.id : '-'}
+                            />
+                            <Dropdown
+                                label='TYYPPI'
+                                disabled={true}
+                                selected={endNode.type}
+                                codeList={nodeTypeCodeList}
+                            />
+                            <InputContainer
+                                label='NIMI'
+                                disabled={true}
+                                value={endNode && endNode.stop ? endNode.stop!.nameFi : '-'}
+                            />
+                        </div>
                     </div>
-                    <div className={s.inputLabel}>
-                        VIIM. LINKIN LOPPUSOLMU SARAKE NRO
+                    <div className={s.flexRow}>
+                        <div className={s.formItem}>
+                            <div className={s.inputLabel}>
+                                VERKKO
+                            </div>
+                            <TransitToggleButtonBar
+                                selectedTransitTypes={[link!.transitType]}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className={s.flexRow}>
-                    <div className={s.flexInnerRow}>
-                        <input
-                            placeholder='1'
-                            type='text'
-                            className={s.smallInput}
-                        />
-                        <Checkbox
-                            checked={false}
-                            text={'Ohitusaika kirja-aikat.'}
-                            onClick={this.onChange}
-                        />
-                    </div>
-                    <div className={s.flexInnerRow}>
-                        <input
-                            placeholder='1'
-                            type='text'
-                            className={s.smallInput}
-                        />
-                        <Checkbox
-                            checked={false}
-                            text={'Ohitusaika kirja-aikat.'}
-                            onClick={this.onChange}
-                        />
-                    </div>
-                </div>
-                <div className={s.flexRow}>
-                    <div className={s.flexInnerRow}>
-                        <input
-                            placeholder='1'
-                            type='text'
-                            className={s.smallInput}
-                        />
-                        <Checkbox
-                            checked={false}
-                            text={'Ohitusaika nettiaikat.'}
-                            onClick={this.onChange}
-                        />
-                    </div>
-                    <div className={s.flexInnerRow}>
-                        <input
-                            placeholder='1'
-                            type='text'
-                            className={s.smallInput}
-                        />
-                        <Checkbox
-                            checked={false}
-                            text={'Ohitusaika nettiaikat.'}
-                            onClick={this.onChange}
-                        />
-                    </div>
-                </div>
-                <div className={s.flexRow}>
-                    <div className={s.flexGrow}>
+                    <div className={s.flexRow}>
                         <Dropdown
-                            label='SOLMU HASTUS-PAIKKANA'
-                            onChange={this.onChange}
-                            items={['Kyllä', 'Ei']}
-                            selected={'Kyllä'}
+                            label='SUUNTA'
+                            disabled={isEditingDisabled}
+                            selected={link.direction}
+                            codeList={directionCodeList}
+                        />
+                        <InputContainer
+                            label='OS. NRO'
+                            disabled={isEditingDisabled}
+                            value={link.osNumber}
+                        />
+                        <InputContainer
+                            label='LINKIN PITUUS (m)'
+                            disabled={isEditingDisabled}
+                            value={link.length}
                         />
                     </div>
-                    <div className={s.flexFiller} />
+                    <div className={s.flexRow}>
+                        <InputContainer
+                            label='KATU'
+                            disabled={isEditingDisabled}
+                            value={link.streetName}
+                        />
+                        <InputContainer
+                            label='KATUOSAN OS. NRO'
+                            disabled={isEditingDisabled}
+                            value={link.streetNumber}
+                        />
+                        <Dropdown
+                            onChange={this.onChange}
+                            disabled={isEditingDisabled}
+                            codeList={municipalityCodeList}
+                            selected={link.municipalityCode}
+                            label='KUNTA'
+                        />
+                    </div>
+                    <div className={s.flexRow}>
+                        <div className={s.inputLabel}>
+                            ALKUSOLMUN SARAKE NRO
+                        </div>
+                        <div className={s.inputLabel}>
+                            VIIM. LINKIN LOPPUSOLMU SARAKE NRO
+                        </div>
+                    </div>
+                    <div className={s.flexRow}>
+                        <div className={s.flexInnerRow}>
+                            <input
+                                placeholder='1'
+                                type='text'
+                                className={s.smallInput}
+                            />
+                            <Checkbox
+                                checked={false}
+                                text={'Ohitusaika kirja-aikat.'}
+                                onClick={this.onChange}
+                            />
+                        </div>
+                        <div className={s.flexInnerRow}>
+                            <input
+                                placeholder='1'
+                                type='text'
+                                className={s.smallInput}
+                            />
+                            <Checkbox
+                                checked={false}
+                                text={'Ohitusaika kirja-aikat.'}
+                                onClick={this.onChange}
+                            />
+                        </div>
+                    </div>
+                    <div className={s.flexRow}>
+                        <div className={s.flexInnerRow}>
+                            <input
+                                placeholder='1'
+                                type='text'
+                                className={s.smallInput}
+                            />
+                            <Checkbox
+                                checked={false}
+                                text={'Ohitusaika nettiaikat.'}
+                                onClick={this.onChange}
+                            />
+                        </div>
+                        <div className={s.flexInnerRow}>
+                            <input
+                                placeholder='1'
+                                type='text'
+                                className={s.smallInput}
+                            />
+                            <Checkbox
+                                checked={false}
+                                text={'Ohitusaika nettiaikat.'}
+                                onClick={this.onChange}
+                            />
+                        </div>
+                    </div>
+                    <div className={s.flexRow}>
+                        <div className={s.flexGrow}>
+                            <Dropdown
+                                label='SOLMU HASTUS-PAIKKANA'
+                                onChange={this.onChange}
+                                items={['Kyllä', 'Ei']}
+                                selected={'Kyllä'}
+                            />
+                        </div>
+                        <div className={s.flexFiller} />
+                    </div>
+                    <div className={s.flexRow}>
+                        <InputContainer
+                            label='PÄIVITTÄJÄ'
+                            value={link.modifiedBy}
+                        />
+                        <InputContainer
+                            label='PÄIVITYSPVM'
+                            value={Moment(link.modifiedOn)
+                            .format(datetimeStringDisplayFormat)}
+                        />
+                    </div>
                 </div>
-                <div className={s.flexRow}>
-                    <InputContainer
-                        label='PÄIVITTÄJÄ'
-                        value={link.modifiedBy}
-                    />
-                    <InputContainer
-                        label='PÄIVITYSPVM'
-                        value={Moment(link.modifiedOn)
-                          .format(datetimeStringDisplayFormat)}
-                    />
+                <MultiTabTextarea
+                    tabs={['Tariffialueet', 'Määränpäät', 'Ajoajat']}
+                />
+                <div className={s.buttonBar}>
+                    <Button
+                        onClick={this.navigateToNode(link.startNode.id)}
+                        type={ButtonType.ROUND}
+                    >
+                        Alkusolmu
+                    </Button>
+                    <Button
+                        onClick={this.navigateToNode(link.endNode.id)}
+                        type={ButtonType.ROUND}
+                    >
+                        Loppusolmu
+                    </Button>
                 </div>
             </div>
-            <MultiTabTextarea
-                tabs={['Tariffialueet', 'Määränpäät', 'Ajoajat']}
-            />
-            <div className={s.buttonBar}>
-                <Button
-                    onClick={this.navigateToNode(link.startNode.id)}
-                    type={ButtonType.ROUND}
-                >
-                    Alkusolmu
-                </Button>
-                <Button
-                    onClick={this.navigateToNode(link.endNode.id)}
-                    type={ButtonType.ROUND}
-                >
-                    Loppusolmu
-                </Button>
-            </div>
+            <Button
+                type={ButtonType.SAVE}
+                disabled={isSaveButtonDisabled}
+                onClick={this.save}
+            >
+                Tallenna muutokset
+            </Button>
         </div>
         );
     }
