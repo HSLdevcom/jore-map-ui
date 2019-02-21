756ec3ede2ea6f2036742b3411a5276294263495
Joe Niemi <niemijoe@hotmail.com>
2019-02-21 14:26:42 +0200

    617 Fixed routePath list items: showing route name instead of pysäkki (#644)
    * removed unused vstate variable
    
    * refactored routePathListTab views
    
    * renamed routePathListObject -> routePathListItem
    
    * Showing name of stop instead of 'Pysäkki', highlighting stops with lightblue
    
    * getGeometry func prop --> geometry prop
    
    * removed unused variable
    
    * using NodeType
    
    * removed duplicate NodeType

---
+++ src/components/sidebar/routePathView/routePathListTab/RoutePathListItem.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import classnames from 'classnames';
+import * as L from 'leaflet';
+import { inject, observer } from 'mobx-react';
+import { MapStore } from '~/stores/mapStore';
+import { RoutePathStore } from '~/stores/routePathStore';
+import * as s from './routePathListItem.scss';
+
+interface IRoutePathListItemProps {
+    mapStore?: MapStore;
+    routePathStore?: RoutePathStore;
+    id: string;
+    geometry: L.LatLng[];
+    shadowClass?: string;
+    header: JSX.Element;
+    body: JSX.Element;
+    listIcon: JSX.Element;
+    reference: React.RefObject<HTMLDivElement>;
+}
+
+@inject('routePathStore', 'mapStore')
+@observer
+class RoutePathListItem
+    extends React.Component<IRoutePathListItemProps> {
+    private toggleIsExtended = () => {
+        this.props.routePathStore!.toggleExtendedObject(this.props.id);
+
+        if (this.props.routePathStore!.isObjectExtended(this.props.id)) {
+            this.props.mapStore!.setMapBounds(
+                this.getBounds(),
+            );
+        }
+    }
+
+    private getBounds = () => {
+        const bounds:L.LatLngBounds = new L.LatLngBounds([]);
+        this.props.geometry.forEach((geom: L.LatLng) => bounds.extend(geom));
+        return bounds;
+    }
+
+    private onMouseEnter = () => {
+        this.props.routePathStore!.setHighlightedObject(this.props.id);
+    }
+
+    private onMouseLeave = () => {
+        if (this.props.routePathStore!.isObjectHighlighted(this.props.id)) {
+            this.props.routePathStore!.setHighlightedObject(null);
+        }
+    }
+
+    render() {
+        const isExtended = this.props.routePathStore!.isObjectExtended(
+            this.props.id,
+        );
+        return (
+            <div
+                ref={this.props.reference}
+                className={classnames(
+                    s.item,
+                    this.props.shadowClass,
+                )}
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+            >
+                <div className={s.listIcon}>
+                    {this.props.listIcon}
+                </div>
+                <div onClick={this.toggleIsExtended}>
+                    {this.props.header}
+                </div>
+                { isExtended &&
+                    <div className={s.itemContent}>
+                        {this.props.body}
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default RoutePathListItem;
