4c981db158a4a6922f4ca0f916c57c12c55c6ea6
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-21 12:32:08 +0200

    Remove unused file

--- src/models/IEvent.ts
+++
@@ -1,8 +0,0 @@
-import eventType from '~/enums/eventType';
-import endpoints from '~/enums/endpoints';
-
-export default interface IEvent {
-    entity: endpoints;
-    action: eventType;
-    preObject: object;
-}
