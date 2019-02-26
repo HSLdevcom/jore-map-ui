baa9d5c7a30c73312d1d6b20464653db650d153f
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-26 10:31:53 +0200

    PR fixes

--- src/validation/models/stopValidationModel.ts
+++ src/validation/models/stopValidationModel.ts
@@ -1,7 +1,7 @@
-const nodeValidationModel = {
+const stopValidatorModel = {
     name: 'required|min:1|max:20|string', // TODO: These are just made up
     radius: 'required|numeric|min:0|max:99999',
 
 };
 
-export default nodeValidationModel;
+export default stopValidatorModel;
