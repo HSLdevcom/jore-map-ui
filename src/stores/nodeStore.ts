d792697e0ead927545586bba36824143ddf2edb0
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-02-26 12:23:22 +0200

    Reset node store on save

--- src/stores/nodeStore.ts
+++ src/stores/nodeStore.ts
@@ -56,6 +56,12 @@ export class NodeStore {
     }
 
     @action
+    public setCurrentStateAsOld = () => {
+        this._oldLinks = _.cloneDeep(this._links);
+        this._oldNode = _.cloneDeep(this.node);
+    }
+
+    @action
     public changeLinkGeometry = (latLngs: L.LatLng[], index: number) => {
         if (!this._node) throw new Error('Node was null.'); // Should not occur
 
