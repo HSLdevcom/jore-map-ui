e1f0cd22f5e2a708fe7bf5c76a9c266cca332df3
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-03-11 11:04:54 +0200

    Moving coordinateView to class component seems to fix race condition problem

--- src/components/sidebar/networkView/nodeView/NodeCoordinatesView.tsx
+++ src/components/sidebar/networkView/nodeView/NodeCoordinatesView.tsx
@@ -15,64 +15,68 @@ interface INodeCoordinatesViewProps {
     onChangeCoordinates: (coordinates: L.LatLng) => void;
 }
 
-const getCoordinateSpecificData = (locationType: NodeLocationType, nodeType: NodeType) => {
-    let iconClassName = '';
-    let label = '';
+class NodeCoordinatesView extends React.Component<INodeCoordinatesViewProps> {
+    getCoordinateSpecificData = (locationType: NodeLocationType, nodeType: NodeType) => {
+        let iconClassName = '';
+        let label = '';
 
-    switch (locationType) {
-    case 'coordinates':
-        iconClassName = NodeTypeHelper.getTypeClass(nodeType, true);
-        label = 'Mitattu piste';
-        break;
-    case 'coordinatesManual':
-        iconClassName = s.manual;
-        label = 'Sovitettu piste';
-        break;
-    case 'coordinatesProjection':
-        iconClassName = s.projected;
-        label = 'Projektoitu piste';
-        break;
+        switch (locationType) {
+        case 'coordinates':
+            iconClassName = NodeTypeHelper.getTypeClass(nodeType, true);
+            label = 'Mitattu piste';
+            break;
+        case 'coordinatesManual':
+            iconClassName = s.manual;
+            label = 'Sovitettu piste';
+            break;
+        case 'coordinatesProjection':
+            iconClassName = s.projected;
+            label = 'Projektoitu piste';
+            break;
+        }
+
+        return { iconClassName, label };
     }
 
-    return { iconClassName, label };
-};
-
-const nodeCoordinatesView = (props: INodeCoordinatesViewProps) => {
-    const latChange = (v: string) => {
+    latChange = (v: string) => {
         const lat = Number(v);
-        props.onChangeCoordinates(new L.LatLng(lat, props.coordinates.lng));
-    };
-    const lonChange = (v: string) => {
+        this.props.onChangeCoordinates(new L.LatLng(lat, this.props.coordinates.lng));
+    }
+
+    lonChange = (v: string) => {
         const lon = Number(v);
-        props.onChangeCoordinates(new L.LatLng(props.coordinates.lat, lon));
-    };
+        this.props.onChangeCoordinates(new L.LatLng(this.props.coordinates.lat, lon));
+    }
 
-    const { iconClassName, label } = getCoordinateSpecificData(props.locationType, props.nodeType);
+    render() {
+        const { iconClassName, label } =
+            this.getCoordinateSpecificData(this.props.locationType, this.props.nodeType);
 
-    return (
-        <div className={s.nodeCoordinatesView}>
-            <div className={s.nodeCoordinatesHeader}>
-                {label}
-                <div className={classnames(s.labelIcon, iconClassName)}/>
+        return (
+            <div className={s.nodeCoordinatesView}>
+                <div className={s.nodeCoordinatesHeader}>
+                    {label}
+                    <div className={classnames(s.labelIcon, iconClassName)}/>
+                </div>
+                <div className={s.coordinatesInputs}>
+                    <InputContainer
+                        value={this.props.coordinates.lat}
+                        onChange={this.latChange}
+                        label={'Latitude'}
+                        type='number'
+                        disabled={this.props.isEditingDisabled}
+                    />
+                    <InputContainer
+                        value={this.props.coordinates.lng}
+                        onChange={this.lonChange}
+                        label={'Longitude'}
+                        type='number'
+                        disabled={this.props.isEditingDisabled}
+                    />
+                </div>
             </div>
-            <div className={s.coordinatesInputs}>
-                <InputContainer
-                    value={props.coordinates.lat}
-                    onChange={latChange}
-                    label={'Latitude'}
-                    type='number'
-                    disabled={props.isEditingDisabled}
-                />
-                <InputContainer
-                    value={props.coordinates.lng}
-                    onChange={lonChange}
-                    label={'Longitude'}
-                    type='number'
-                    disabled={props.isEditingDisabled}
-                />
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
-export default nodeCoordinatesView;
+export default NodeCoordinatesView;
