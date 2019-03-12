8c86dc33649e5234f4e3ea88458a5a14310a7707
niemijoe <joe.niemi@cgi.com>
2019-03-12 15:40:40 +0200

    Fixed lat lng container positions, added TODO comment

--- src/models/baseModels/INodeBase.ts
+++ src/models/baseModels/INodeBase.ts
@@ -2,6 +2,6 @@ import NodeType from '~/enums/nodeType';
 
 export default interface INodeBase {
     id: string;
-    shortId?: string;
+    shortId?: string; // TODO: split as identifierLetter identifierString (solkirjain sollistunnus)
     type: NodeType;
 }
