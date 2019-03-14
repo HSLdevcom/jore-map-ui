36364cd452774ea0b54a0c9f8b74b43d486cf035
Joakim Kronqvist <kronken@users.noreply.github.com>
2019-03-14 16:25:53 +0200

    677 route path geometry should be required (#704)
    * Routepath is invalid if empty geometry
    
    * set updated routePath as initial instead of original
    
    * Refactoring isGeometryValid
    
    * Clone routepath when setting old routepath

--- src/util/geomValidator.ts
+++ src/util/geomValidator.ts
@@ -1,6 +1,7 @@
 import { IRoutePathLink } from '~/models';
 
 const validateRoutePathLinks = (rpLinks: IRoutePathLink[]) => {
+    if (rpLinks.length === 0) return false;
     return rpLinks.every((rpLink, index) => (
         index === 0 || rpLinks[index - 1].endNode.id === rpLink.startNode.id));
 };
