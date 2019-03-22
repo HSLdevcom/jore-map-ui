59aaabf7fbcd766637720b9b12aaa421f5b1b490
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-03-22 13:54:13 +0200

    Test error with errorCode to get 100% test coverage on error Store

--- src/stores/__tests__/errorStore.test.ts
+++ src/stores/__tests__/errorStore.test.ts
@@ -1,4 +1,6 @@
 import { ErrorStore } from '../errorStore';
+import IError from '~/models/IError';
+import httpStatusDescriptionCodeList from '~/codeLists/httpStatusDescriptionCodeList';
 
 describe('errorStore.addError', () => {
     it('Adds one error to error store', () => {
@@ -44,6 +46,35 @@ describe('errorStore.addError', () => {
         expect(errorStore.errorCount).toEqual(1);
         expect(errorStore.latestError).toContain(errorMessage);
         expect(errorStore.latestError).toContain(exceptionMessage);
+        expect(console.error).toBeCalled();
+        // Clean up console.error to be back to normal
+        console.error = consoleError;
+        // tslint:enable:no-console
+    });
+
+    it('Adds one error with http status description', () => {
+        // tslint:disable:no-console
+        // Mocks console.error
+        const consoleError = console.error;
+        global.console.error = jest.fn();
+
+        const errorStore = new ErrorStore;
+        const errorMessage = 'This is a test error';
+        const exceptionMessage = 'This is a test exception message';
+        const errorException: IError = {
+            message: exceptionMessage,
+            name: 'Test exception',
+            errorCode: 409,
+        };
+
+        errorStore.addError(errorMessage, errorException);
+
+        expect(errorStore.errorCount).toEqual(1);
+
+        const error = errorStore.pop();
+
+        expect(error).toContain(httpStatusDescriptionCodeList[409]);
+        expect(errorStore.errorCount).toEqual(0);
         expect(console.error).toBeCalled();
         // Clean up console.error to be back to normal
         console.error = consoleError;
