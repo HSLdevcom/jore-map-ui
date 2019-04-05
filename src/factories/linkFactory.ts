a5cc1e757d66504da173c4676a53830ce707b73a
niemijoe <joe.niemi@cgi.com>
2019-04-05 13:07:53 +0300

    rename as mapExternalLink

--- src/factories/linkFactory.ts
+++ src/factories/linkFactory.ts
@@ -4,7 +4,7 @@ import IExternalLink from '~/models/externals/IExternalLink';
 import NodeFactory from './nodeFactory';
 
 class LinkFactory {
-    public static createLinkFromExternalLink = (externalLink: IExternalLink): ILink => {
+    public static mapExternalLink = (externalLink: IExternalLink): ILink => {
         const geoJson = JSON.parse(externalLink.geojson);
 
         return {
