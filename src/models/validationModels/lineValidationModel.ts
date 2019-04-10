2437e80076e3de5921a9da1d04cb192da7a12bab
niemijoe <joe.niemi@cgi.com>
2019-04-10 15:42:32 +0300

    Fixed line validation model

--- src/models/validationModels/lineValidationModel.ts
+++ src/models/validationModels/lineValidationModel.ts
@@ -1,15 +1,15 @@
 const linkValidationModel = {
-    lintunnus: 'required|min:4|max:6|string',
-    linperusreitti: 'required|min:4|max:6|string',
-    linvoimast: 'required|min:1|max:99999|string', // TODO: timestamp max length?
-    linvoimviimpvm: 'required|min:1|max:99999|string', // TODO: timestamp max length?
-    linjoukkollaji: 'required|min:2|max:2|string',
-    lintilorg: 'required|min:3|max:3|string',
-    linverkko: 'required|min:1|max:1|string',
-    linryhma: 'required|min:3|max:3|string',
-    linjlkohde: 'required|min:1|max:6|string',
-    vaihtoaika: 'required|min:0|max:99999|numeric', // TODO: integer max length?
-    linkorvtyyppi: 'required|min:0|max:2|string',
+    id: 'required|min:4|max:6|string',
+    lineBasicRoute: 'required|min:4|max:6|string',
+    lineStartDate: 'required|min:1|max:99999|string', // TODO: timestamp max length?
+    lineEndDate: 'required|min:1|max:99999|string', // TODO: timestamp max length?
+    publicTransportType: 'required|min:2|max:2|string',
+    clientOrganization: 'required|min:3|max:3|string',
+    transitType: 'required|min:1|max:1|string',
+    lineGroup: 'required|min:3|max:3|string',
+    publicTransportDestination: 'required|min:1|max:6|string',
+    exchangeTime: 'required|min:0|max:99999|numeric', // TODO: integer max length?
+    lineReplacementType: 'required|min:0|max:2|string',
 };
 
 export default linkValidationModel;
