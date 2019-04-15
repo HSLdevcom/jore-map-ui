1c3bf2c5e1c290cecc0811f15fa75a3c0870cae1
niemijoe <joe.niemi@cgi.com>
2019-04-15 15:23:04 +0300

    removed extra wrapping div

--- src/components/sidebar/lineView/LineInfoTab.tsx
+++ src/components/sidebar/lineView/LineInfoTab.tsx
@@ -116,116 +116,114 @@ class LineInfoTab extends React.Component<ILineInfoTabProps, ILineInfoTabState>{
 
         return (
         <div className={classnames(s.lineInfoTabView, s.form)}>
-            <div className={s.content}>
-                <div className={s.formSection}>
-                    <div className={s.flexRow}>
-                        <div className={s.formItem}>
-                            <div className={s.inputLabel}>
-                                VERKKO
-                            </div>
-                            <TransitToggleButtonBar
-                                selectedTransitTypes={selectedTransitTypes}
-                                toggleSelectedTransitType={this.selectTransitType}
-                                disabled={!this.props.isNewLine}
-                                errorMessage={!line!.transitType ?
-                                    'Verkon tyyppi täytyy valita.' : undefined}
-                            />
+            <div className={s.formSection}>
+                <div className={s.flexRow}>
+                    <div className={s.formItem}>
+                        <div className={s.inputLabel}>
+                            VERKKO
                         </div>
-                    </div>
-                    <div className={s.flexRow}>
-                        <InputContainer
-                            disabled={isUpdating}
-                            label='LINJAN TUNNUS'
-                            value={line.id}
-                            onChange={this.onChangeLineId}
-                            validationResult={invalidPropertiesMap['id']}
-                        />
-                        <InputContainer
-                            disabled={isEditingDisabled}
-                            label='LINJAN PERUS REITTI'
-                            value={line.lineBasicRoute}
-                            onChange={onChange('lineBasicRoute')}
-                            validationResult={invalidPropertiesMap['lineBasicRoute']}
+                        <TransitToggleButtonBar
+                            selectedTransitTypes={selectedTransitTypes}
+                            toggleSelectedTransitType={this.selectTransitType}
+                            disabled={!this.props.isNewLine}
+                            errorMessage={!line!.transitType ?
+                                'Verkon tyyppi täytyy valita.' : undefined}
                         />
                     </div>
-                    <div className={s.flexRow}>
-                        <InputContainer
-                            disabled={isEditingDisabled}
-                            label='LINJAN VOIMAANASTUMISPÄIVÄ'
-                            type='date'
-                            value={line.lineStartDate}
-                            onChange={this.onChangeStartDate}
-                            validationResult={invalidPropertiesMap['lineStartDate']}
-                        />
-                    </div>
-                    <div className={s.flexRow}>
-                        <InputContainer
-                            disabled={isEditingDisabled}
-                            label='LINJAN VIIMEINEN VOIMASSAOLOPÄIVÄ'
-                            type='date'
-                            value={line.lineEndDate}
-                            onChange={this.onChangeEndDate}
-                            validationResult={invalidPropertiesMap['lineEndDate']}
-                        />
-                    </div>
-                    <div className={s.flexRow}>
-                        <Dropdown
-                            label='JOUKKOLIIKENNELAJI'
-                            disabled={isEditingDisabled}
-                            selected={line.publicTransportType}
-                            emptyItem={{
-                                value: '',
-                                label: '',
-                            }}
-                            items={this.props.codeListStore!.getCodeList('Joukkoliikennelaji')}
-                            onChange={onChange('publicTransportType')}
-                            validationResult={invalidPropertiesMap['publicTransportType']}
-                        />
-                        <Dropdown
-                            label='TILAAJAORGANISAATIO'
-                            disabled={isEditingDisabled}
-                            selected={line.clientOrganization}
-                            items={this.props.codeListStore!.getCodeList('Tilaajaorganisaatio')}
-                            onChange={onChange('clientOrganization')}
-                            validationResult={invalidPropertiesMap['clientOrganization']}
-                        />
-                    </div>
-                    <div className={s.flexRow}>
-                        <Dropdown
-                            label='JOUKKOLIIKENNEKOHDE'
-                            disabled={isEditingDisabled}
-                            selected={line.publicTransportDestination}
-                            emptyItem={{
-                                value: '',
-                                label: '',
-                            }}
-                            items={this.props.codeListStore!.getCodeList('Joukkoliikennekohde')}
-                            onChange={onChange('publicTransportDestination')}
-                            validationResult={invalidPropertiesMap['publicTransportDestination']}
-                        />
-                        <Dropdown
-                            label='LINJAN KORVAAVA TYYPPI'
-                            disabled={isEditingDisabled}
-                            selected={line.lineReplacementType}
-                            emptyItem={{
-                                value: '',
-                                label: '',
-                            }}
-                            items={this.props.codeListStore!.getCodeList('LinjanKorvaavaTyyppi')}
-                            onChange={onChange('lineReplacementType')}
-                            validationResult={invalidPropertiesMap['lineReplacementType']}
-                        />
-                    </div>
-                    <div className={s.flexRow}>
-                        <InputContainer
-                            disabled={isEditingDisabled}
-                            label='VAIHTOAJAN PIDENNYS'
-                            type='number'
-                            value={line.exchangeTime}
-                            onChange={onChange('exchangeTime')}
-                            validationResult={invalidPropertiesMap['exchangeTime']}
-                        />
-                    </div>
+                </div>
+                <div className={s.flexRow}>
+                    <InputContainer
+                        disabled={isUpdating}
+                        label='LINJAN TUNNUS'
+                        value={line.id}
+                        onChange={this.onChangeLineId}
+                        validationResult={invalidPropertiesMap['id']}
+                    />
+                    <InputContainer
+                        disabled={isEditingDisabled}
+                        label='LINJAN PERUS REITTI'
+                        value={line.lineBasicRoute}
+                        onChange={onChange('lineBasicRoute')}
+                        validationResult={invalidPropertiesMap['lineBasicRoute']}
+                    />
+                </div>
+                <div className={s.flexRow}>
+                    <InputContainer
+                        disabled={isEditingDisabled}
+                        label='LINJAN VOIMAANASTUMISPÄIVÄ'
+                        type='date'
+                        value={line.lineStartDate}
+                        onChange={this.onChangeStartDate}
+                        validationResult={invalidPropertiesMap['lineStartDate']}
+                    />
+                </div>
+                <div className={s.flexRow}>
+                    <InputContainer
+                        disabled={isEditingDisabled}
+                        label='LINJAN VIIMEINEN VOIMASSAOLOPÄIVÄ'
+                        type='date'
+                        value={line.lineEndDate}
+                        onChange={this.onChangeEndDate}
+                        validationResult={invalidPropertiesMap['lineEndDate']}
+                    />
+                </div>
+                <div className={s.flexRow}>
+                    <Dropdown
+                        label='JOUKKOLIIKENNELAJI'
+                        disabled={isEditingDisabled}
+                        selected={line.publicTransportType}
+                        emptyItem={{
+                            value: '',
+                            label: '',
+                        }}
+                        items={this.props.codeListStore!.getCodeList('Joukkoliikennelaji')}
+                        onChange={onChange('publicTransportType')}
+                        validationResult={invalidPropertiesMap['publicTransportType']}
+                    />
+                    <Dropdown
+                        label='TILAAJAORGANISAATIO'
+                        disabled={isEditingDisabled}
+                        selected={line.clientOrganization}
+                        items={this.props.codeListStore!.getCodeList('Tilaajaorganisaatio')}
+                        onChange={onChange('clientOrganization')}
+                        validationResult={invalidPropertiesMap['clientOrganization']}
+                    />
+                </div>
+                <div className={s.flexRow}>
+                    <Dropdown
+                        label='JOUKKOLIIKENNEKOHDE'
+                        disabled={isEditingDisabled}
+                        selected={line.publicTransportDestination}
+                        emptyItem={{
+                            value: '',
+                            label: '',
+                        }}
+                        items={this.props.codeListStore!.getCodeList('Joukkoliikennekohde')}
+                        onChange={onChange('publicTransportDestination')}
+                        validationResult={invalidPropertiesMap['publicTransportDestination']}
+                    />
+                    <Dropdown
+                        label='LINJAN KORVAAVA TYYPPI'
+                        disabled={isEditingDisabled}
+                        selected={line.lineReplacementType}
+                        emptyItem={{
+                            value: '',
+                            label: '',
+                        }}
+                        items={this.props.codeListStore!.getCodeList('LinjanKorvaavaTyyppi')}
+                        onChange={onChange('lineReplacementType')}
+                        validationResult={invalidPropertiesMap['lineReplacementType']}
+                    />
+                </div>
+                <div className={s.flexRow}>
+                    <InputContainer
+                        disabled={isEditingDisabled}
+                        label='VAIHTOAJAN PIDENNYS'
+                        type='number'
+                        value={line.exchangeTime}
+                        onChange={onChange('exchangeTime')}
+                        validationResult={invalidPropertiesMap['exchangeTime']}
+                    />
                 </div>
             </div>
         </div>
