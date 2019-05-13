ce466bb7943adf7f2ff54fb0ac764e673a12862b
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-05-13 09:22:40 +0300

    Add regex validation and simple tests also to FE

---
+++ src/validation/__tests__/regexRules.test.ts
@@ -0,0 +1,21 @@
+import FormValidator from '../FormValidator';
+import regexRules from '../regexRules';
+
+// Very simple testing that regex rules work, main testing is done in backend.
+describe('Validator.validate - regexRules', () => {
+    it('works with valid input using rule: upperCaseOrNumbersOrSpace', () => {
+        const validationResult = FormValidator.validate(
+            'A123',
+            regexRules.upperCaseOrNumbersOrSpace
+        );
+        expect(validationResult.isValid).toEqual(true);
+    });
+
+    it('works with invalid input using rule: upperCaseOrNumbersOrSpace', () => {
+        const validationResult = FormValidator.validate(
+            'a123',
+            regexRules.upperCaseOrNumbersOrSpace
+        );
+        expect(validationResult.isValid).toEqual(false);
+    });
+});
