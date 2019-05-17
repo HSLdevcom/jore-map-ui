d57e271dc7bcf7909178881cba7b4b954ac56127
niemijoe <joe.niemi@cgi.com>
2019-05-17 14:10:21 +0300

    fixed error msg

--- src/codeLists/textCodeList.ts
+++ src/codeLists/textCodeList.ts
@@ -1,6 +1,6 @@
 const textCodeList = {
     login_failed_no_jore_username:
-        'Kirjautuminen epäonnistui, käyttäjänimeä ei löytynyt jore tietokannasta HSL-id:stä saadun sähköpostiosoitteen ${email} avulla.',
+        'Kirjautuminen epäonnistui, käyttäjänimeä ei löytynyt joukkoliikennerekisterin tietokannasta HSL-id:stä saadun sähköpostiosoitteen ${email} avulla.  Ota yhteyttä joukkoliikennerekisterin käyttäjätunnusten hallinnoijaan.',
     no_access_token:
         'Kirjautuminen epäonnistui, HSL-id:stä ei antanut access tokenia.'
 };
