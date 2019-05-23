9bfbf7ae91b2aac9a234694d9e6dce05fdd19639
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2019-05-23 10:00:47 +0300

    Add validation model interface to route path

--- src/models/validationModels/routePathValidationModel.ts
+++ src/models/validationModels/routePathValidationModel.ts
@@ -1,4 +1,5 @@
 import Constants from '~/constants/constants';
+import { IRoutePath } from '..';
 
 const originRule = 'required|min:1|max:20|string';
 const destinationRule = 'required|min:1|max:20|string';
@@ -6,10 +7,19 @@ const nameRule = 'required|min:1|max:60|string';
 const shortNameRule = 'required|min:1|max:20|string';
 const dateRule = 'required|date';
 
-const routePathValidationModel = {
+type RoutePathKeys = keyof IRoutePath;
+type IRoutePathValidationModel = { [key in RoutePathKeys]: string };
+
+const routePathValidationModel: IRoutePathValidationModel = {
     routeId: 'required|min:4|max:6|string',
     direction: 'required|min:1|max:1|string',
     startTime: dateRule,
+    internalId: '',
+    color: '',
+    visible: '',
+    transitType: '',
+    lineId: '',
+    routePathLinks: '',
     endTime: dateRule,
     originFi: originRule,
     originSw: originRule,
@@ -22,7 +32,9 @@ const routePathValidationModel = {
     nameSw: nameRule,
     isStartNodeUsingBookSchedule: 'boolean',
     startNodeBookScheduleColumnNumber: 'numeric|min:0|max:99',
-    exceptionPath: 'min:1|max:1|string'
+    exceptionPath: 'min:1|max:1|string',
+    modifiedBy: '',
+    modifiedOn: ''
 };
 
 export default routePathValidationModel;
