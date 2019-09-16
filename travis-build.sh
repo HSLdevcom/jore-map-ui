5372691b1c1b6f446bf8a6f4d2deeab435dc51c7
niemijoe <joe.niemi@cgi.com>
2019-09-16 14:50:20 +0300

    Fixed tag

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+# Note: values to these variables are defined at travis: https://travis-ci.org/HSLdevcom/jore-map-ui/settings
+
 ORG=${ORG:-hsldevcom}
 APP_BUILD_DATE=$(date +'%d.%m.%Y')
 
@@ -8,7 +10,7 @@ DOCKER_TAG=':latest'
 if [[ $TRAVIS_BRANCH == "develop" ]]; then
   DOCKER_TAG=':develop'
 fi
-DOCKER_IMAGE=$ORG/jore-map-ui:${DOCKER_TAG}
+DOCKER_IMAGE=$ORG/jore-map-ui${DOCKER_TAG}
 
 docker build --build-arg BACKEND_API_URL=${API_URL} --build-arg BACKEND_GEOSERVER_URL=${GEOSERVER_URL} --build-arg APP_BUILD_DATE=${APP_BUILD_DATE} --tag=$DOCKER_IMAGE .
 
