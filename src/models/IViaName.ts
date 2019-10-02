aa7579f10493236979de58a2100d02c6a48dbe09
niemijoe <joe.niemi@cgi.com>
2019-10-02 15:57:06 +0300

    Fields as optional

--- src/models/IViaName.ts
+++ src/models/IViaName.ts
@@ -3,10 +3,10 @@ interface IViaNamePrimaryKey {
 }
 
 export default interface IViaName extends IViaNamePrimaryKey {
-    destinationFi1: string;
-    destinationFi2: string;
-    destinationSw1: string;
-    destinationSw2: string;
+    destinationFi1?: string;
+    destinationFi2?: string;
+    destinationSw1?: string;
+    destinationSw2?: string;
 }
 
 export { IViaName, IViaNamePrimaryKey };
