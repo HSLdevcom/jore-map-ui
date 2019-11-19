4ab8cfae9359ded66283ec099d17d51233073924
niemijoe <joe.niemi@cgi.com>
2019-11-19 13:08:47 +0200

    Removed empty lines

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -96,7 +96,6 @@ module.exports = {
       '.jsx',
     ],
     alias: {
-
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -145,7 +144,6 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-
               compact: true,
             },
           },
