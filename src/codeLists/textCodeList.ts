c1a74021ff9d4d0b3e0b8bb3d11719409c33b5fe
niemijoe <joe.niemi@cgi.com>
2019-11-22 15:03:47 +0200

    Added error msg when user is not verified

--- src/codeLists/textCodeList.ts
+++ src/codeLists/textCodeList.ts
@@ -1,4 +1,6 @@
 const textCodeList = {
+    login_failed_username_not_verified:
+        'Kirjautuminen epäonnistui. Käyttäjätunnustasi ei ole vielä hyväksytty HSL:n puolesta.',
     login_failed_no_jore_username:
         'Kirjautuminen epäonnistui, käyttäjänimeä ei löytynyt joukkoliikennerekisterin tietokannasta HSL-id:stä saadun sähköpostiosoitteen ${email} avulla.  Ota yhteyttä joukkoliikennerekisterin käyttäjätunnusten hallinnoijaan.',
     no_access_token: 'Kirjautuminen epäonnistui, HSL-id:stä ei antanut access tokenia.'
