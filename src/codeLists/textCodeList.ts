c16ff52ba338b04fe48c42d13b22a19ab9c92364
niemijoe <joe.niemi@cgi.com>
2019-11-26 13:05:56 +0200

    Error key fix

--- src/codeLists/textCodeList.ts
+++ src/codeLists/textCodeList.ts
@@ -1,5 +1,5 @@
 const textCodeList = {
-    login_failed_username_has_no_access_permission:
+    login_failed_user_has_no_access_permission:
         'Kirjautuminen epäonnistui. Käyttäjätunnustasi ei ole vielä hyväksytty HSL:n puolesta.',
     login_failed_no_jore_username:
         'Kirjautuminen epäonnistui, käyttäjänimeä ei löytynyt joukkoliikennerekisterin tietokannasta HSL-id:stä saadun sähköpostiosoitteen ${email} avulla.  Ota yhteyttä joukkoliikennerekisterin käyttäjätunnusten hallinnoijaan.',
