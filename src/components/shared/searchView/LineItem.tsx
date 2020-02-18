da91d8692792b2992a6575fedc3223eae1f57d7d
Joe Niemi <niemijoe@hotmail.com>
2020-02-18 14:33:11 +0200

    Open all routes button + searchResults css (#1346)

--- src/components/shared/searchView/LineItem.tsx
+++ src/components/shared/searchView/LineItem.tsx
@@ -4,6 +4,9 @@ import ReactMoment from 'react-moment';
 import ISearchLine from '~/models/searchModels/ISearchLine';
 import ISearchLineRoute from '~/models/searchModels/ISearchLineRoute';
 import navigator from '~/routing/navigator';
+import QueryParams from '~/routing/queryParams';
+import routeBuilder from '~/routing/routeBuilder';
+import SubSites from '~/routing/subSites';
 import searchStore from '~/stores/searchStore';
 import NavigationUtils from '~/utils/NavigationUtils';
 import TransitTypeUtils from '~/utils/TransitTypeUtils';
@@ -43,28 +46,47 @@ class LineItem extends React.Component<ILineItemProps> {
         );
     }
 
-    public render() {
+    private openAllRoutes = () => {
+        const routesLinkBuilder = routeBuilder.to(SubSites.routes);
+        this.props.line.routes.forEach(route => {
+            routesLinkBuilder.append(QueryParams.routes, route.id);
+        });
+        const routesLink = routesLinkBuilder.toLink();
+        searchStore.setSearchInput('');
+        navigator.goTo({
+            link: routesLink
+        });
+    };
+
+    render() {
         return (
             <div className={s.lineItemView}>
-                <div
-                    className={s.lineItem}
-                    onClick={() => NavigationUtils.openLineView({ lineId: this.props.line.id })}
-                    data-cy='lineItem'
-                >
-                    <div className={s.icon}>
-                        <TransitIcon
-                            transitType={this.props.line.transitType}
-                            isWithoutBox={false}
-                        />
-                    </div>
+                <div className={s.lineContainer}>
                     <div
-                        className={classNames(
-                            TransitTypeUtils.getColorClass(this.props.line.transitType),
-                            s.lineLabel
-                        )}
+                        className={s.lineItem}
+                        onClick={() => NavigationUtils.openLineView({ lineId: this.props.line.id })}
+                        data-cy='lineItem'
                     >
-                        {this.props.line.id}
+                        <div className={s.icon}>
+                            <TransitIcon
+                                transitType={this.props.line.transitType}
+                                isWithoutBox={false}
+                            />
+                        </div>
+                        <div
+                            className={classNames(
+                                TransitTypeUtils.getColorClass(this.props.line.transitType),
+                                s.lineLabel
+                            )}
+                        >
+                            {this.props.line.id}
+                        </div>
                     </div>
+                    {this.props.line.routes.length > 0 && (
+                        <div className={s.openAllRoutesButton} onClick={this.openAllRoutes}>
+                            Avaa reitit ({this.props.line.routes.length})
+                        </div>
+                    )}
                 </div>
                 {this.props.line.routes
                     .slice()
