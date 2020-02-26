a5b772a865c120d664d953d4cff5110c78468ea1
Joe Niemi <niemijoe@hotmail.com>
2020-02-26 14:24:59 +0200

    Removed nodeSass (#1366)

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -71,9 +71,7 @@ module.exports = {
         publicPath: publicPath,
         // Point sourcemap entries to original disk location (format as URL on Windows)
         devtoolModuleFilenameTemplate: info =>
-            path
-                .relative(paths.appSrc, info.absoluteResourcePath)
-                .replace(/\\/g, '/')
+            path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/')
     },
     resolve: {
         // This allows you to set a fallback for where Webpack should look for modules.
@@ -172,9 +170,7 @@ module.exports = {
                         use: [
                             'style-loader', // creates style nodes from JS strings
                             {
-                                loader: require.resolve(
-                                    'typings-for-css-modules-loader'
-                                ),
+                                loader: require.resolve('typings-for-css-modules-loader'),
                                 options: {
                                     modules: true,
                                     namedExport: true,
@@ -184,7 +180,13 @@ module.exports = {
                                     sourceMap: shouldUseSourceMap
                                 }
                             },
-                            'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    // Prefer `dart-sass`
+                                    implementation: require('sass')
+                                }
+                            } // compiles Sass to CSS, using Dart Sass
                         ]
                     },
                     // The notation here is somewhat confusing.
@@ -212,9 +214,7 @@ module.exports = {
                                     },
                                     use: [
                                         {
-                                            loader: require.resolve(
-                                                'css-loader'
-                                            ),
+                                            loader: require.resolve('css-loader'),
                                             options: {
                                                 importLoaders: 1,
                                                 minimize: true,
@@ -222,9 +222,7 @@ module.exports = {
                                             }
                                         },
                                         {
-                                            loader: require.resolve(
-                                                'postcss-loader'
-                                            ),
+                                            loader: require.resolve('postcss-loader'),
                                             options: {
                                                 // Necessary for external CSS imports to work
                                                 // https://github.com/facebookincubator/create-react-app/issues/2677
@@ -260,12 +258,7 @@ module.exports = {
                         // it's runtime that would otherwise processed through "file" loader.
                         // Also exclude `html` and `json` extensions so they get processed
                         // by webpacks internal loaders.
-                        exclude: [
-                            /\.(js|jsx|mjs)$/,
-                            /\.html$/,
-                            /\.json$/,
-                            /\.scss$/
-                        ],
+                        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
                         options: {
                             name: 'static/media/[name].[hash:8].[ext]'
                         }
