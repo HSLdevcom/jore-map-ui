5d8e9a38ae2eecf58ad9e5f285fb1fdc7f103226
niemijoe <joe.niemi@cgi.com>
2020-04-15 10:01:56 +0300

    Render multiple transitType borders if they exist

--- src/components/shared/TransitTypeNodeIcon.tsx
+++ src/components/shared/TransitTypeNodeIcon.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import NodeType from '~/enums/nodeType';
 import TransitType from '~/enums/transitType';
 import TransitTypeUtils from '~/utils/TransitTypeUtils';
+import commonStyles from './styles/common.scss';
 import * as s from './transitTypeNodeIcon.scss';
 
 interface ITransitTypeNodeIconProps {
@@ -13,40 +14,48 @@ interface ITransitTypeNodeIconProps {
 }
 
 class TransitTypeNodeIcon extends React.Component<ITransitTypeNodeIconProps> {
+    private addBorder = (nodeIcon: React.ReactNode, color: string, opacity: number) => {
+        return (
+            <div className={s.iconBorder} style={{ opacity, borderColor: color }}>
+                {nodeIcon}
+            </div>
+        );
+    };
+
     render() {
         const { nodeType, transitTypes, isDisabled, isTimeAlignmentStop } = this.props;
-        let color: string = '';
         let opacity: number = 1;
-        const nodeIcon: React.ReactNode = (
+        if (isDisabled && nodeType === NodeType.STOP) {
+            opacity = 0.5;
+        }
+
+        let nodeIcon: React.ReactNode = (
             <div
                 className={classnames(
                     s.nodeIcon,
+                    nodeType === NodeType.STOP && transitTypes && transitTypes.length > 1
+                        ? s.smallSize
+                        : s.normalSize,
                     isTimeAlignmentStop ? s.timeAlignmentIcon : undefined
                 )}
             />
         );
 
         if (nodeType === NodeType.MUNICIPALITY_BORDER) {
-            color = '#c900ff';
+            nodeIcon = this.addBorder(nodeIcon, '#c900ff', opacity);
         } else if (nodeType === NodeType.CROSSROAD) {
-            color = '#727272';
+            nodeIcon = this.addBorder(nodeIcon, '#727272', opacity);
         } else {
-            if (isDisabled) {
-                opacity = 0.5;
+            if (transitTypes && transitTypes.length > 0) {
+                transitTypes!.forEach(type => {
+                    nodeIcon = this.addBorder(nodeIcon, TransitTypeUtils.getColor(type), opacity);
+                });
+            } else {
+                nodeIcon = this.addBorder(nodeIcon, commonStyles.unusedNodeColor, opacity);
             }
-            transitTypes!.forEach(type => {
-                color = TransitTypeUtils.getColor(type);
-            });
         }
 
-        return (
-            <div
-                className={s.transitTypeNodeIcon}
-                style={{ borderColor: color, opacity: opacity ? opacity : 1 }}
-            >
-                {nodeIcon}
-            </div>
-        );
+        return nodeIcon;
     }
 }
 
