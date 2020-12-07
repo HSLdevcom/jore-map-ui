918d1f7d15d20ffa4897af025a303d84525e0f69
niemijoe <joe.niemi@cgi.com>
2020-12-07 13:29:39 +0200

    Disable SelectNetworkEntity with a delay when dragging link vertex point in node/link view

--- src/helpers/EventListener.ts
+++ src/helpers/EventListener.ts
@@ -12,7 +12,6 @@ type eventName =
     | 'mapClick'
     | 'nodeClick'
     | 'networkNodeClick'
-    | 'networkLinkClick'
     | 'geometryChange'
     | 'routePathNodeClick'
     | 'routePathLinkClick'
