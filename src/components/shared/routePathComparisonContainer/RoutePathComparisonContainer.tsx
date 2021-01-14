e4a0760d7e05e0bf0d3f0d1173bedc2a494d7299
niemijoe <joe.niemi@cgi.com>
2021-01-14 16:20:08 +0200

    Fix displaying routePath comparison values

--- src/components/shared/routePathComparisonContainer/RoutePathComparisonContainer.tsx
+++ src/components/shared/routePathComparisonContainer/RoutePathComparisonContainer.tsx
@@ -1,4 +1,4 @@
-import { forOwn, omit } from 'lodash';
+import { forOwn, pick } from 'lodash';
 import { inject, observer } from 'mobx-react';
 import React, { useEffect, useState } from 'react';
 import propertyCodeLists from '~/codeLists/propertyCodeLists';
@@ -14,35 +14,42 @@ interface IRoutePathComparisonContainerProps {
     routePathComparisonStore?: RoutePathComparisonStore;
 }
 
-const excludedRoutePathProperties = [
-    'lineId',
-    'routeId',
-    'direction',
-    'startDate',
-    'routePathLinks',
-    'modifiedOn',
-    'modifiedBy',
-] as const;
-
 interface IComparableRoutePath
-    extends Omit<
+    extends Pick<
         IRoutePath,
-        | 'lineId'
-        | 'routeId'
-        | 'direction'
-        | 'startDate'
-        | 'routePathLinks'
-        | 'modifiedOn'
-        | 'modifiedBy'
+        | 'nameFi'
+        | 'nameSw'
+        | 'originFi'
+        | 'destinationFi'
+        | 'originSw'
+        | 'destinationSw'
+        | 'shortNameFi'
+        | 'shortNameSw'
+        | 'length'
+        | 'exceptionPath'
     > {}
+
+const comparableRoutePathProperties = [
+    'nameFi',
+    'nameSw',
+    'originFi',
+    'destinationFi',
+    'originSw',
+    'destinationSw',
+    'shortNameFi',
+    'shortNameSw',
+    'length',
+    'exceptionPath',
+] as const;
 
 const RoutePathComparisonContainer = inject('routePathComparisonStore')(
     observer((props: IRoutePathComparisonContainerProps) => {
         const { routePath1, routePath2 } = props;
         const [areEqualPropertiesVisible, setEqualPropertiesVisible] = useState<boolean>(false);
         const [areCrossroadsVisible, setCrossroadsVisible] = useState<boolean>(false);
-        const rp1: IComparableRoutePath = omit(routePath1, excludedRoutePathProperties);
-        const rp2: IComparableRoutePath = omit(routePath2, excludedRoutePathProperties);
+        const comparableRp1: IComparableRoutePath = pick(routePath1, comparableRoutePathProperties);
+        const comparableRp2: IComparableRoutePath = pick(routePath2, comparableRoutePathProperties);
+
         useEffect(() => {
             props.routePathComparisonStore!.setRoutePath1(routePath1);
             props.routePathComparisonStore!.setRoutePath2(routePath2);
@@ -53,14 +60,24 @@ const RoutePathComparisonContainer = inject('routePathComparisonStore')(
         const renderRoutePathDifference = () => {
             const rows: any = [];
             let index: number = 0;
-            forOwn(rp1, (value1, key) => {
-                const value2 = rp2[key];
+
+            const _getValue = (value: any, property: string) => {
+                if (property === 'exceptionPath') {
+                    return value === '0' ? 'Ei' : 'Kyllä';
+                }
+                return value;
+            };
+
+            forOwn(comparableRp1, (rawValue1: any, property: string) => {
+                const value1 = _getValue(rawValue1, property);
+                const rawValue2 = comparableRp2[property];
+                const value2 = _getValue(rawValue2, property);
                 const areValuesEqual = getAreValuesEqual(value1, value2);
                 if (areEqualPropertiesVisible || !areValuesEqual) {
                     rows.push(
                         <ComparableRow
                             key={`row-${index}`}
-                            label={propertyCodeLists['routePath'][key]}
+                            label={propertyCodeLists['routePath'][property]}
                             value1={value1}
                             value2={value2}
                         />
