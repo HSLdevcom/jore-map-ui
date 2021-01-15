80df8de00e9a5a1c56fee6c23b18d5406fc55db9
Joe Niemi <niemijoe@hotmail.com>
2021-01-15 17:14:42 +0200

    RoutePathView compare button is now clickable (#1847)

--- src/components/controls/Button.tsx
+++ src/components/controls/Button.tsx
@@ -1,7 +1,9 @@
 import classnames from 'classnames';
-import { observer } from 'mobx-react';
+import { isEmpty } from 'lodash';
+import { inject, observer } from 'mobx-react';
 import React from 'react';
 import ButtonType from '~/enums/buttonType';
+import { AlertStore } from '~/stores/alertStore';
 import * as s from './button.scss';
 
 interface IButtonProps {
@@ -17,64 +19,87 @@ interface IButtonProps {
     hasBorderRadius?: boolean;
     hasNoTransition?: boolean;
     hasNoMargin?: boolean;
+    clickPreventedNotification?: string;
+    alertStore?: AlertStore;
 }
 
-const Button = observer((props: IButtonProps) => {
-    const getTypeClass = (type?: ButtonType) => {
-        switch (type) {
-            case ButtonType.SQUARE: {
-                return undefined; // default button type is square
+const Button = inject('alertStore')(
+    observer((props: IButtonProps) => {
+        const getTypeClass = (type?: ButtonType) => {
+            switch (type) {
+                case ButtonType.SQUARE: {
+                    return undefined; // default button type is square
+                }
+                case ButtonType.ROUND: {
+                    return s.round;
+                }
             }
-            case ButtonType.ROUND: {
-                return s.round;
+            return undefined;
+        };
+
+        const showClickPreventedNotification = (notification: string) => {
+            props.alertStore!.setNotificationMessage({
+                message: notification,
+            });
+        };
+
+        const {
+            children,
+            onClick,
+            type,
+            className,
+            title,
+            disabled,
+            isWide,
+            hasPadding,
+            hasReverseColor,
+            hasNoTransition,
+            hasBorderRadius,
+            hasNoMargin,
+            clickPreventedNotification,
+            alertStore,
+            ...attrs
+        } = props;
+
+        const _onClick = () => {
+            if (!isEmpty(clickPreventedNotification)) {
+                showClickPreventedNotification(clickPreventedNotification!);
+                return;
             }
+            if (!disabled) {
+                onClick();
+            }
+        };
+
+        let disabledClass;
+        if (!isEmpty(clickPreventedNotification)) {
+            disabledClass = s.disabledClickable;
+        } else if (disabled) {
+            disabledClass = s.disabled;
         }
-        return undefined;
-    };
 
-    const {
-        children,
-        onClick,
-        type,
-        className,
-        title,
-        disabled,
-        isWide,
-        hasPadding,
-        hasReverseColor,
-        hasNoTransition,
-        hasBorderRadius,
-        hasNoMargin,
-        ...attrs
-    } = props;
-
-    const _onClick = () => {
-        if (!disabled) {
-            onClick();
-        }
-    };
-
-    return (
-        <div
-            {...attrs}
-            className={classnames(
-                s.button,
-                className,
-                getTypeClass(type),
-                disabled ? s.disabled : undefined,
-                isWide ? s.wide : undefined,
-                hasPadding ? s.hasPadding : undefined,
-                hasReverseColor ? s.reverseColor : undefined,
-                hasNoTransition ? undefined : s.transition,
-                hasBorderRadius ? s.borderRadius : undefined,
-                hasNoMargin ? s.hasNoMargin : undefined
-            )}
-            onClick={_onClick}
-            title={title ? title : ''}
-        >
-            {children}
-        </div>
-    );
-});
+        return (
+            <div
+                {...attrs}
+                className={classnames(
+                    s.button,
+                    className,
+                    getTypeClass(type),
+                    disabledClass,
+                    isWide ? s.wide : undefined,
+                    hasPadding ? s.hasPadding : undefined,
+                    hasReverseColor ? s.reverseColor : undefined,
+                    hasNoTransition ? undefined : s.transition,
+                    hasBorderRadius ? s.borderRadius : undefined,
+                    hasNoMargin ? s.hasNoMargin : undefined
+                )}
+                onClick={_onClick}
+                title={title ? title : ''}
+            >
+                {children}
+            </div>
+        );
+    })
+);
 
 export default Button;
