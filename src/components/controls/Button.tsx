f28ffe9d3be53a7a00aa9fa9b023abf029cb025a
Joe Niemi <niemijoe@hotmail.com>
2021-01-19 12:58:50 +0200

    Removed usage of alertStore from Button component (#1849)

--- src/components/controls/Button.tsx
+++ src/components/controls/Button.tsx
@@ -1,9 +1,7 @@
 import classnames from 'classnames';
-import { isEmpty } from 'lodash';
-import { inject, observer } from 'mobx-react';
+import { observer } from 'mobx-react';
 import React from 'react';
 import ButtonType from '~/enums/buttonType';
-import { AlertStore } from '~/stores/alertStore';
 import * as s from './button.scss';
 
 interface IButtonProps {
@@ -19,87 +17,75 @@ interface IButtonProps {
     hasBorderRadius?: boolean;
     hasNoTransition?: boolean;
     hasNoMargin?: boolean;
-    clickPreventedNotification?: string;
-    alertStore?: AlertStore;
+    onDisabledButtonClick?: () => void;
 }
 
-const Button = inject('alertStore')(
-    observer((props: IButtonProps) => {
-        const getTypeClass = (type?: ButtonType) => {
-            switch (type) {
-                case ButtonType.SQUARE: {
-                    return undefined; // default button type is square
-                }
-                case ButtonType.ROUND: {
-                    return s.round;
-                }
+const Button = observer((props: IButtonProps) => {
+    const getTypeClass = (type?: ButtonType) => {
+        switch (type) {
+            case ButtonType.SQUARE: {
+                return undefined; // default button type is square
             }
-            return undefined;
-        };
-
-        const showClickPreventedNotification = (notification: string) => {
-            props.alertStore!.setNotificationMessage({
-                message: notification,
-            });
-        };
-
-        const {
-            children,
-            onClick,
-            type,
-            className,
-            title,
-            disabled,
-            isWide,
-            hasPadding,
-            hasReverseColor,
-            hasNoTransition,
-            hasBorderRadius,
-            hasNoMargin,
-            clickPreventedNotification,
-            alertStore,
-            ...attrs
-        } = props;
-
-        const _onClick = () => {
-            if (!isEmpty(clickPreventedNotification)) {
-                showClickPreventedNotification(clickPreventedNotification!);
-                return;
+            case ButtonType.ROUND: {
+                return s.round;
             }
-            if (!disabled) {
-                onClick();
-            }
-        };
-
-        let disabledClass;
-        if (!isEmpty(clickPreventedNotification)) {
-            disabledClass = s.disabledClickable;
-        } else if (disabled) {
-            disabledClass = s.disabled;
         }
+        return undefined;
+    };
 
-        return (
-            <div
-                {...attrs}
-                className={classnames(
-                    s.button,
-                    className,
-                    getTypeClass(type),
-                    disabledClass,
-                    isWide ? s.wide : undefined,
-                    hasPadding ? s.hasPadding : undefined,
-                    hasReverseColor ? s.reverseColor : undefined,
-                    hasNoTransition ? undefined : s.transition,
-                    hasBorderRadius ? s.borderRadius : undefined,
-                    hasNoMargin ? s.hasNoMargin : undefined
-                )}
-                onClick={_onClick}
-                title={title ? title : ''}
-            >
-                {children}
-            </div>
-        );
-    })
-);
+    const {
+        children,
+        onClick,
+        type,
+        className,
+        title,
+        disabled,
+        isWide,
+        hasPadding,
+        hasReverseColor,
+        hasNoTransition,
+        hasBorderRadius,
+        hasNoMargin,
+        onDisabledButtonClick,
+        ...attrs
+    } = props;
+
+    const _onClick = () => {
+        if (onDisabledButtonClick) {
+            onDisabledButtonClick();
+        } else if (!disabled) {
+            onClick();
+        }
+    };
+
+    let disabledClass;
+    if (onDisabledButtonClick) {
+        disabledClass = s.disabledClickable;
+    } else if (disabled) {
+        disabledClass = s.disabled;
+    }
+
+    return (
+        <div
+            {...attrs}
+            className={classnames(
+                s.button,
+                className,
+                getTypeClass(type),
+                disabledClass,
+                isWide ? s.wide : undefined,
+                hasPadding ? s.hasPadding : undefined,
+                hasReverseColor ? s.reverseColor : undefined,
+                hasNoTransition ? undefined : s.transition,
+                hasBorderRadius ? s.borderRadius : undefined,
+                hasNoMargin ? s.hasNoMargin : undefined
+            )}
+            onClick={_onClick}
+            title={title ? title : ''}
+        >
+            {children}
+        </div>
+    );
+});
 
 export default Button;
