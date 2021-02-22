f176ad1a92aef4dbfe03e50e5bf06f124bf3e611
niemijoe <joe.niemi@cgi.com>
2021-02-22 09:34:35 +0200

    RpLinkList node click: center to both start and end node

--- src/components/sidebar/routePathView/routePathListTab/RoutePathListNode.tsx
+++ src/components/sidebar/routePathView/routePathListTab/RoutePathListNode.tsx
@@ -210,10 +210,17 @@ const RoutePathListNode = inject(
                 if (isExtended) {
                     props.routePathLayerStore!.setExtendedListItemId(null);
                 } else {
+                    const rpLinks = props.routePathStore!.routePath!.routePathLinks;
+                    const nextRpLink = rpLinks[routePathLink.orderNumber - 2];
                     props.routePathLayerStore!.setExtendedListItemId(currentListItemId);
                     const geometry = props.routePathStore!.getLinkGeom(routePathLink.id);
                     const bounds: L.LatLngBounds = new L.LatLngBounds([]);
                     geometry.forEach((geom: L.LatLng) => bounds.extend(geom));
+                    // Extend bounds with nextRpLink if it's found
+                    if (nextRpLink) {
+                        const nextRpLinkGeom = props.routePathStore!.getLinkGeom(nextRpLink.id);
+                        nextRpLinkGeom.forEach((geom: L.LatLng) => bounds.extend(geom));
+                    }
                     props.mapStore!.setMapBounds(bounds);
                 }
             };
