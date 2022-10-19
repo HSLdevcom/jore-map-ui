2981cff2f54a1fa7b405f978845a4d1af3fda155
niemijoe <joe.niemi@cgi.com>
2022-10-19 14:55:50 +0300

    Remove deprecated test

--- cypress/integration/routeListView.spec.js
+++ cypress/integration/routeListView.spec.js
@@ -13,23 +13,6 @@ describe('RouteListView tests - read access user', () => {
         cy.getTestElement('sidebarHeaderView').find('[data-cy=closeButton]').first().click();
         cy.getTestElement('routeListView').should('not.exist');
     });
-
-    it('Can open a second route to routeList and close one route', () => {
-        cy.getTestElement('lineSearch').click();
-        cy.getTestElement('lineSearch').type('550');
-        cy.wait(1000);
-
-        cy.getTestElement('routeItem').first().click();
-
-        cy.getTestElement('editButton').should('not.exist');
-        cy.getTestElement('routeListView').should('exist');
-        cy.getTestElement('routeListView').find('[data-cy=routeId]').should('have.length', 2);
-
-        cy.getTestElement('sidebarHeaderView').find('[data-cy=closeButton]').first().click();
-
-        cy.getTestElement('routeListView').should('exist');
-        cy.getTestElement('routeListView').find('[data-cy=routeId]').should('have.length', 1);
-    });
 });
 
 describe('RouteListView tests - write access user', () => {
